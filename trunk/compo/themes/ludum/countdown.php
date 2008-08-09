ba505e22dc2b4a86651a1b7cdb807d767476ab9f
philhassey <philhassey@a6a53419-064e-0410-8e23-07da878cd106>
2008-08-09 03:06:08 +0000

    times

--- trunk/compo/themes/ludum/countdown.php
+++ trunk/compo/themes/ludum/countdown.php
@@ -23,7 +23,8 @@
 //countdown(2008,7,6,12+6,0); // Compo End Time //
 
 // LD 12
-countdown(2008,8,8,12+10,0); // Start Time //
+//countdown(2008,8,8,12+10,0); // Start Time //
+countdown(2008,8,10,12+10,0); // Start Time //
 
 //--------------------------
 // author: Louai Munajim
