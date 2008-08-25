2e58eb55076f214f4493c2a6d39b80d3bc21e890
povrazor <povrazor@a6a53419-064e-0410-8e23-07da878cd106>
2008-08-25 03:33:27 +0000

    Didn't like the heading stuff.

--- hub/hub-index.php
+++ hub/hub-index.php
@@ -37,17 +37,18 @@ define('MAGPIE_CACHE_DIR', 'hub/cache');
 							?>
 						</td>
 						<td align="center" bgcolor="#90745e">
+							<!--
 							<font size="-1">Next Event - <strong>Ludum Dare 13 - December 2008</strong></font><br />
 							Mini LD48 #3 - <strong>Friday September 5th, 2008</strong> - Hosted by PoV
-							<!--
+							
 							<font size="+1"><b>Ludum Dare 12 Countdown</b> - <font color="#fbda81">
 							<?php
 							require 'compo/wp-content/themes/ludum/countdown.php';
 							?>
 							</font></font>
+							<br />
+							<br />
 							-->
-							<br />
-							<br />
 							
 							<font size="+1">
 							<?php
