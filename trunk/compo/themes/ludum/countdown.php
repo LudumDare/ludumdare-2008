006bd9a9d6a6e19e8ce4ecc2779b5af84243965b
mikekasprzak <mikekasprzak@a6a53419-064e-0410-8e23-07da878cd106>
2009-04-18 02:55:24 +0000

    Ludum Dare countdown clock for the 48 hours of the competition.

--- trunk/compo/themes/ludum/countdown.php
+++ trunk/compo/themes/ludum/countdown.php
@@ -33,8 +33,8 @@
 //countdown(2008,12,21,12+10,0); // Judge End Time //
 
 // LD 14
-countdown(2009,4,17,12+10,0); // Start Time //
-//countdown(2009,4,19,12+10,0); // End Time //
+//countdown(2009,4,17,12+10,0); // Start Time //
+countdown(2009,4,19,12+10,0); // End Time //
 //countdown(2009,5,3,12+10,0); // Judge End Time //
 
 //--------------------------
