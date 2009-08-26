b4ebfcd164619cc2a53e658cf102b2ab0db66880
philhassey <philhassey@a6a53419-064e-0410-8e23-07da878cd106>
2009-08-26 02:26:04 +0000

    just messin

--- compo/themes/ludum/fncs.php
+++ compo/themes/ludum/fncs.php
@@ -7,8 +7,8 @@ function my_get_buttons() {
         $my_auth = get_the_author_meta('login');
         $cat = array_pop(get_the_category());
         $my_cat = $cat->slug;
-        print_r($cat);
-        $cid = $cat->ID;
+//         print_r($cat);
+        $cid = $cat->cat_ID;
 //         echo "$my_auth : $my_cat : $is_final";
         $my_link = get_option('home')."/?category_name=".urlencode($my_cat)."&author_name=".urlencode($my_auth);
         $state = get_option("compo-$cid-state");
