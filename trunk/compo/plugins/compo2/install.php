0e3b1796f3bc145339fed1d60053c2ad655b98c6
philhassey <philhassey@a6a53419-064e-0410-8e23-07da878cd106>
2009-08-27 22:05:56 +0000

    compo2 plugin

---
+++ trunk/compo/plugins/compo2/install.php
@@ -0,0 +1,55 @@
+<?php
+function compo2_install() {
+    global $compo2;
+    $key = $compo2["version.key"];
+    $cur = get_option($key);
+    
+    
+    
+/*    $version = 3;
+    if ($cur < $version) { 
+        compo2_query("create table c2_results (cid int, uid int, data blob)");
+        update_option($key,$version);
+    }*/
+    
+    $version = 7;
+    if ($cur < $version) {
+        compo2_query("create table c2_entry (id int not null auto_increment primary key, cid int, uid int, notes text, links blob, data blob)");
+        update_option($key,$version);
+    }
+    $version = 8;
+    if ($cur < $version) {
+        compo2_query("create table c2_rate (cid int, to_uid int, from_uid int, data blob, )");
+        update_option($key,$version);
+    }
+    $version = 9;
+    if ($cur < $version) {
+        compo2_query("alter table c2_entry add results blob");
+        update_option($key,$version);
+    }
+    $version = 11;
+    if ($cur < $version) {
+        compo2_query("alter table c2_entry add active int default 0");
+        update_option($key,$version);
+    }
+    
+    $version = 12;
+    if ($cur < $version) {
+        compo2_query("alter table c2_entry add title varchar(255)");
+        update_option($key,$version);
+    }
+    
+    $version = 13;
+    if ($cur < $version) {
+        compo2_query("alter table c2_entry add shots blob");
+        update_option($key,$version);
+    }
+    $version = 14;
+    if ($cur < $version) {
+        compo2_query("alter table c2_rate add comments text");
+        update_option($key,$version);
+    }
+}
+
+register_activation_hook($GLOBALS["compo2"]["plugin"],"compo2_install");
+?>
---
+++ trunk/compo/plugins/compo2/rate.php
@@ -0,0 +1,131 @@
+<?php
+
+function _compo2_rate($params) {
+    if (!$params["uid"]) { return _compo2_preview($params); }
+
+    $action = isset($_REQUEST["action"])?$_REQUEST["action"]:"default";
+    
+    if ($action == "default") {
+        return _compo2_rate_list($params);
+    } elseif ($action == "rate") {
+        return _compo2_rate_rate($params);
+    } elseif ($action == "submit") {
+        return _compo2_rate_submit($params);
+    } elseif ($action == "edit") {
+        return _compo2_active_form($params);
+    } elseif ($action == "save") {
+        return _compo2_active_save($params);
+    }
+}
+
+function _compo2_rate_sort($a,$b) {
+    return strcmp($a["s"],$b["s"]);
+}
+
+function _compo2_rate_list($params) {
+    $r = compo2_query("select * from c2_entry where cid = ? and active = 1",array($params["cid"]));
+    foreach ($r as $k=>$ce) {
+        $r[$k]["s"] = md5("{$params["uid"]}|{$ce["cid"]}|{$ce["uid"]}");
+    }
+    usort($r,"_compo2_rate_sort");
+    $n=0;
+    echo "<table>";
+    echo "<tr><th><th>C";
+    foreach ($params["cats"] as $k) { echo "<th>".substr($k,0,3); }
+    foreach ($r as $ce) {
+        if ($ce["uid"] == $params["uid"]) { continue; }
+        $ve = array_pop(compo2_query("select * from c2_rate where cid = ? and to_uid = ? and from_uid = ?",array($params["cid"],$ce["uid"],$params["uid"])));
+        $ue = compo2_get_user($ce["uid"]);
+        echo "<tr>";
+        echo "<td><a href='?action=rate&uid={$ce["uid"]}'>".htmlentities($ue->user_nicename)."</a>";
+        echo "<td>".(strlen($ve["comments"])?"x":"-");
+        $data = unserialize($ve["data"]);
+        foreach ($params["cats"] as $k) {
+            echo "<td align=center>".(compo_number_format($data[$k]));
+        }
+        $n += 1;
+        if ($n >= 20 && !strlen($_REQUEST["more"])) { break; }
+    }
+    echo "</table>";
+    
+    if (!strlen($_REQUEST["more"])) {
+        echo "<p><a href='?more=1'>Show all entries.</a></p>";
+    }
+    
+    $ce = compo2_entry_load($params["cid"],$params["uid"]);
+    if ($ce["id"]) { echo "<p><a href='?action=edit'>Edit your entry.</a></p>"; }
+}
+
+function _compo2_rate_rate($params) {
+    $uid = intval($_REQUEST["uid"]);
+    _compo2_preview_show($params,$uid);
+    $ce = compo2_entry_load($params["cid"],$uid);
+    $ve = array_pop(compo2_query("select * from c2_rate where cid = ? and to_uid = ? and from_uid = ?",array($params["cid"],$ce["uid"],$params["uid"])));
+    echo "<h2>Rate this Entry</h2>";
+    echo "<form method=post action='?action=submit&uid=$uid'>";
+    echo "<p>";
+    echo "<table>";
+    $data = unserialize($ve["data"]);
+    foreach ($params["cats"] as $k) {
+        echo "<tr><th>".htmlentities($k);
+        echo "<td>";
+        $v = $data[$k];
+        compo2_select("data[$k]",array(""=>"n/a","5"=>"5 - Best","4"=>"4","3"=>"3","2"=>"2","1"=>"1 - Worst"),$v);
+    }
+    echo "</table>";
+    echo "</p>";
+    echo "<h3>Comments (non-anonymous)</h3>";
+    echo "<textarea name='comments' rows=4 cols=60>".htmlentities($ve["comments"])."</textarea>";
+    echo "<p><input type='submit' value='Save'></p>";
+    echo "</form>";
+    
+}
+
+function _compo2_rate_submit($params) {
+    $uid = intval($_REQUEST["uid"]);
+    $ce = compo2_entry_load($params["cid"],$uid);
+    compo2_query("delete from c2_rate where cid = ? and to_uid = ? and from_uid = ?",array($params["cid"],$ce["uid"],$params["uid"]));
+    $data = array();
+    foreach ($_REQUEST["data"] as $k=>$v) {
+        $data[$k] = strlen($v)?intval($v):"";
+    }
+    
+    compo2_insert("c2_rate",array(
+        "cid"=>$params["cid"],
+        "to_uid"=>$ce["uid"],
+        "from_uid"=>$params["uid"],
+        "data"=>serialize($data),
+        "comments"=>$_REQUEST["comments"],
+    ));
+    
+    _compo2_rate_recalc($params,$ce["uid"]);
+    
+    header("Location: ?action=default"); die;
+}
+
+
+function _compo2_rate_recalc($params,$uid) {
+    $cid = $params["cid"];
+    $ce = compo2_entry_load($params["cid"],$uid);
+    $r = compo2_query("select * from c2_rate where cid = ? and to_uid = ?",array($cid,$uid));
+    
+    $data = array();
+    foreach ($params["cats"] as $k) {
+        $value = 0;
+        $total = 0;
+        foreach ($r as $ve) {
+            if ($ve["from_uid"] == $uid) { continue; } // no voting for self
+            $dd = unserialize($ve["data"]);
+            if (!strlen($dd[$k])) { continue; }
+            $value += intval($dd[$k]);
+            $total += 1;
+        }
+        $data[$k] = ($total>=5?round($value/$total,2):"");
+    }
+    compo2_update("c2_entry",array(
+        "id"=>$ce["id"],
+        "results"=>serialize($data),
+    ));
+}
+
+?>
