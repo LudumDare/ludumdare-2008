212bb52bbe2588e8ecf8b46e5fc1e6702f7da273
philhassey <philhassey@a6a53419-064e-0410-8e23-07da878cd106>
2009-08-31 22:14:46 +0000

    code shuffling

--- compo/plugins/compo2/results.php
+++ compo/plugins/compo2/results.php
@@ -206,8 +206,8 @@ function _compo2_results_top($params) {
         echo "<td align=center valign=top>";
         echo "<a href='$link'><img src='".compo2_thumb($fname,160,160)."'></a>";
         echo "<td valign=top>";
-        echo "<div>"; _compo2_preview_show_links($ce); echo "</div>";
         echo "<p>".str_replace("\n","<br/>",htmlentities(substr($ce["notes"],0,140)))." ...</p>";
+        echo "<div>"; _compo2_preview_show_links($ce); echo "</div>";
         echo "<td valign=top>";
         asort($e["places"]);
         foreach ($e["places"] as $cat=>$n) {
