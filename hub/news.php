4a24612a58e18165589faa61dfbc7a76e90d6d55
philhassey <philhassey@a6a53419-064e-0410-8e23-07da878cd106>
2009-09-07 16:17:39 +0000

    mabye fixing something

--- hub/news.php
+++ hub/news.php
@@ -2,23 +2,25 @@
 
 require_once 'magpierss2/rss.php';
 
-$rss = fetch_rss($newsfeed);
-
-$NewsItemCount = 0;
-
-foreach ($rss->items as $item) {
-	$title = $item[title];
-	$url   = $item[link];
-	if ($newsprefiximage != '')
-		echo "<img src='$newsprefiximage'>";
-	echo "<a href=$url>$title</a>";
-	if ($newssuffiximage != '')
-		echo "<img src='$newssuffiximage'>";
-	echo "<br />";
-
-	$NewsItemCount++;
-	if ($NewsItemCount == $newsitems)
-		break;
+function show_feed($newsfeed,$newsitems,$newsprefiximage,$newssuffiximage) {
+    $rss = fetch_rss($newsfeed);
+    
+    $NewsItemCount = 0;
+    
+    foreach ($rss->items as $item) {
+        $title = $item[title];
+        $url   = $item[link];
+        if ($newsprefiximage != '')
+            echo "<img src='$newsprefiximage'>";
+        echo "<a href=$url>$title</a>";
+        if ($newssuffiximage != '')
+            echo "<img src='$newssuffiximage'>";
+        echo "<br />";
+    
+        $NewsItemCount++;
+        if ($NewsItemCount == $newsitems)
+            break;
+    }
 }
 
 ?>
