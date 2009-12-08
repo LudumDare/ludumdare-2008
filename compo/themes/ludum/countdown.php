8349ba5912c2a50b1c9aa06f446a2cd62b31377a
philhassey <philhassey@a6a53419-064e-0410-8e23-07da878cd106>
2009-12-08 17:04:03 +0000

    fix

--- compo/themes/ludum/countdown.php
+++ compo/themes/ludum/countdown.php
@@ -38,7 +38,10 @@
 // countdown(2009,5,6,12+10,0); // Judge End Time //
 
 // LD 15
-countdown(2009,8,28,12+10,0); // Start Time //
+//countdown(2009,8,28,12+10,0); // Start Time //
+
+// LD16
+countdown(2009,12,11,12+9,0); // start time
 
 //--------------------------
 // author: Louai Munajim
