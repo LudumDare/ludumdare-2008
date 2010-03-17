48e279c83eb42dd51bc0ba0afd139cb78fa9a6ce
mikekasprzak <mikekasprzak@a6a53419-064e-0410-8e23-07da878cd106>
2010-03-17 02:38:40 +0000

    typo

--- trunk/compo/themes/ludum/archive.php
+++ trunk/compo/themes/ludum/archive.php
@@ -29,13 +29,10 @@
  	  <?php } ?>
 
 
-
-<?php
 		<div class="navigation">
 			<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
 			<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
 		</div>
-?>
 
 <?php
 
