a7a3ce3673d8f18d19ddabb4ce27f854046d2195
philhassey <philhassey@a6a53419-064e-0410-8e23-07da878cd106>
2010-10-03 23:06:10 +0000

    goodz

--- hub/news.php
+++ hub/news.php
@@ -6,6 +6,12 @@ function show_news($newsfeed,$newsitems,$newsprefiximage,$newssuffiximage) {
 	$rss = new SimplePie($newsfeed, dirname(__FILE__) . '/cache');
 	$rss->set_cache_duration ( 60*5 );
 	$rss->init();
+	$rss->handle_content_type();
+	
+	if ($rss->error()) {
+	   echo htmlentities($rss->error());
+	   return;
+    }
 	foreach ($rss->get_items(0, $newsitems) as $item) {
         $title = $item->get_title();
         $url   = $item->get_permalink();
