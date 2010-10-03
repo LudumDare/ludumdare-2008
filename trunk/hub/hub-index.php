0e24a46ea8be24504093e4568e62c3e4fc0daa6d
philhassey <philhassey@a6a53419-064e-0410-8e23-07da878cd106>
2010-10-03 23:16:13 +0000

    goodz

--- trunk/hub/hub-index.php
+++ trunk/hub/hub-index.php
@@ -133,7 +133,7 @@ require_once 'hub/news.php';
 							<font size="-1">
 							<?php
 							$news = 'http://www.ludumdare.com/compo/';
-							$newsfeed = 'http://www.ludumdare.com/compo/feed/rss/';
+							$newsfeed = 'http://www.ludumdare.com/compo/feed/atom/';
 							$newsitems = 5;
 							$newsprefiximage = '/hub/hub-dot.png';
 							
