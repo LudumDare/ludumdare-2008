2817ad7625ac0e75b1d1af9c4de83b7e09b782f4
philhassey <philhassey@a6a53419-064e-0410-8e23-07da878cd106>
2010-10-03 23:03:54 +0000

    goodz

--- trunk/hub/news.php
+++ trunk/hub/news.php
@@ -4,6 +4,7 @@ require_once 'simplepie/simplepie.inc';
 
 function show_news($newsfeed,$newsitems,$newsprefiximage,$newssuffiximage) {
 	$rss = new SimplePie($newsfeed, $_SERVER['DOCUMENT_ROOT'] . '/hub/cache');
+	$rss->set_cache_duration ( 60*5 );
 	foreach ($rss->get_items(0, $newsitems) as $item) {
         $title = $item->get_title();
         $url   = $item->get_permalink();
