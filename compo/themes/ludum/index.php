05b7d363d863d190f48aa48c6e6066c071894297
mikekasprzak <mikekasprzak@a6a53419-064e-0410-8e23-07da878cd106>
2011-04-28 21:04:10 +0000

    spans!!

--- compo/themes/ludum/index.php
+++ compo/themes/ludum/index.php
@@ -36,7 +36,7 @@
 				</div>
 				<?php echo my_get_buttons(); ?>
 
-				<span class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></span>
+				<span class="postmetadata" style="text-align:center" ><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></span>
 				<span style="float: right;border: 1px solid #eee;padding: 2px;background: #fff;"><?php if(function_exists(getILikeThis)) getILikeThis('get'); ?></span>
 			</div>
 
