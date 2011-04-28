13e357e8e17a4917fd40d9ba13f7eeb11d50472d
mikekasprzak <mikekasprzak@a6a53419-064e-0410-8e23-07da878cd106>
2011-04-28 21:44:58 +0000

    it's all einstein... relative.

--- compo/themes/ludum/index.php
+++ compo/themes/ludum/index.php
@@ -38,7 +38,7 @@
 
 				<div style="margin: 0 auto">
 					<div class="postmetadata" style="margin-right: 80px"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></div>
-					<div style="float: right;border: 1px solid #eee;padding: 2px;background: #fff;"><?php if(function_exists(getILikeThis)) getILikeThis('get'); ?></div>
+					<div style="float: right;border: 1px solid #eee;padding: 2px;background: #fff; position: relative;"><?php if(function_exists(getILikeThis)) getILikeThis('get'); ?></div>
 				</div>
 			</div>
 
