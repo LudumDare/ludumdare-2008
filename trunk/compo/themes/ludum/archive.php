5c216e72c232a54c840694d85502c5a60231afb3
mikekasprzak <mikekasprzak@a6a53419-064e-0410-8e23-07da878cd106>
2011-05-03 17:55:24 +0000

    more formatting tweaks of the archive page

--- trunk/compo/themes/ludum/archive.php
+++ trunk/compo/themes/ludum/archive.php
@@ -115,8 +115,7 @@ if (is_author() && is_category()) {
 
 				<p class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' |'); ?><?php if(function_exists(getILikeThis)) getILikeThis('get'); ?> | <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
 				
-				<?php get_tags(); ?>
-
+				<?php /*<?php get_tags(); ?>*/ ?>
 			</div>
 
 		<?php endwhile; ?>
