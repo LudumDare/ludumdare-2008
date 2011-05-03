051f63cc347d59f6a61abf275b0251186f0e6a51
mikekasprzak <mikekasprzak@a6a53419-064e-0410-8e23-07da878cd106>
2011-05-03 16:41:51 +0000

    list eup

--- trunk/compo/themes/ludum/index.php
+++ trunk/compo/themes/ludum/index.php
@@ -31,7 +31,7 @@
 <?php } ?>
 				<div style="float: right;border: 1px solid #eee;padding: 2px;background: #fff;"><?php echo get_avatar(get_the_author_id(),$size='56',$default='' ); ?></div>
 				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-                       		<div>Posted by <?php the_author_posts_link(); ?> <?php $eup = eup_get_extended_profile(); if (($eup->twitter != null) && ($eup->twitter != "")) { echo "(Twitter: @" . $eup->twitter . ")"; } ?></div>
+                       		<div>Posted by <?php the_author_posts_link(); ?> <?php $eup = eup_get_extended_profile(); echo $eup; if (($eup->twitter != null) && ($eup->twitter != "")) { echo "(Twitter: @" . $eup->twitter . ")"; } ?></div>
 				<small><?php the_time('F jS, Y g:i a') ?> <!-- by <?php the_author() ?> --></small>
 
 				<?php echo my_get_buttons(); ?>
