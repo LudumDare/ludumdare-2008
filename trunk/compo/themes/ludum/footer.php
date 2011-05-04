f299d7a8a28a38650149e0c50c569d0cd47a949e
mikekasprzak <mikekasprzak@a6a53419-064e-0410-8e23-07da878cd106>
2011-05-04 19:13:55 +0000

    trying to recolor footer.

--- trunk/compo/themes/ludum/footer.php
+++ trunk/compo/themes/ludum/footer.php
@@ -1,7 +1,7 @@
 <hr />
 <div id="footer">
 <!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
-	<p>
+	<p style="color: #ffffff;">
 		<?php bloginfo('name'); ?> is proudly powered by
 		<a href="http://wordpress.org/">WordPress</a>
 		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
