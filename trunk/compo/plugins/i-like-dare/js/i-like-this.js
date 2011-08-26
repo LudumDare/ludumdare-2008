0afd0b72a9fc847736e15dedc33d167b419cff95
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2011-08-26 07:41:12 +0000

    tweaks... I WANT THIS FIXED FOR REAL!

--- trunk/compo/plugins/i-like-dare/js/i-like-this.js
+++ trunk/compo/plugins/i-like-dare/js/i-like-this.js
@@ -5,8 +5,9 @@ function likeThis(postId) {
 		jQuery.post(blogUrl + "/wp-content/plugins/i-like-dare/like.php",
 			{ id: postId },
 			function(data){
+				jQuery('#iLikeThis-'+postId+' .counter-off').addClass('counter');
 				jQuery('#iLikeThis-'+postId+' .counter').text(data);
-				jQuery('#iLikeThis-'+postId+' .counter').removeAttr('style');				
+//				jQuery('#iLikeThis-'+postId+' .counter').removeAttr('style');
 			});
 	}
 }
