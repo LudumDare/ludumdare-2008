98bc0f64425740b2a5168c4109a1a8c3d99abf26
mikekasprzak <mikekasprzak@a6a53419-064e-0410-8e23-07da878cd106>
2011-09-25 19:49:04 +0000

    wrong date reader.

--- compo/plugins/darenate-plus/darenate-plus.php
+++ compo/plugins/darenate-plus/darenate-plus.php
@@ -434,8 +434,8 @@ if( !class_exists('DarenatePlus') ):
 			global $wpdb;
 			$dplus = get_option( 'DarenatePlus' );
 			$table = $wpdb->prefix . 'expenses';
-			$thedate = $wpdb->get_results("SELECT date FROM $table WHERE status='Completed' ORDER BY ID DESC LIMIT 1");
-			return date('M j, Y \a\t g:i a', strtotime($thedate) );
+			$expense = $wpdb->get_results("SELECT date FROM $table WHERE status='Completed' ORDER BY ID DESC LIMIT 1");
+			return date('M j, Y \a\t g:i a', strtotime($expense->date)) );
 		}
 						
 		function DonorWall($atts=false) {
