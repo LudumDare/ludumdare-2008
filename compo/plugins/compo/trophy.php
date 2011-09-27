7e22553ce0ea8a738802c6685e2a1ea0524291cf
philhassey <philhassey@a6a53419-064e-0410-8e23-07da878cd106>
2011-09-27 16:44:31 +0000

    cache trophy sidebar every 15 minutes

--- compo/plugins/compo/trophy.php
+++ compo/plugins/compo/trophy.php
@@ -91,7 +91,7 @@ require_once dirname(__FILE__)."/../compo2/compo2.php";
 
 function compo_trophy_sidebar() {
     // CACHE ///////////////////////////////////////////////////////////////
-    if (($cres=compo2_cache_read("0",$ckey="compo_trophy_sidebar",15*60))!==false) { echo $cres; return; }
+    if (($cres=compo2_cache_read(0,$ckey="compo_trophy_sidebar",15*60))!==false) { echo $cres; return; }
     ob_start();
     ////////////////////////////////////////////////////////////////////////
 
@@ -111,7 +111,7 @@ function compo_trophy_sidebar() {
     // CACHE ///////////////////////////////////////////////////////////////
     $cres = ob_get_contents();
     ob_end_clean();
-    compo2_cache_write($params["cid"],$ckey,$cres);
+    compo2_cache_write(0,$ckey,$cres);
     echo $cres;
     ////////////////////////////////////////////////////////////////////////
 }
