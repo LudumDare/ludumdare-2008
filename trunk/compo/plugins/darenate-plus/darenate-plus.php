e55c3c7bff519c35990719c01796154126e93755
mikekasprzak <mikekasprzak@a6a53419-064e-0410-8e23-07da878cd106>
2011-09-28 20:38:34 +0000

    checked yo!

--- trunk/compo/plugins/darenate-plus/darenate-plus.php
+++ trunk/compo/plugins/darenate-plus/darenate-plus.php
@@ -614,9 +614,9 @@ $output .= '</select> x
 </p>';
 			endif;		
 			
-			//if( $dplus['enable_wall'] == 1 ):
+			if( $dplus['enable_wall'] == 1 ):
 				$output .= '
-			<p class="recognition_wall"><label><input type="checkbox" id="recognize" name="recognize" value="1" /> '.__('Put my Donation on the Recognition Wall','dplus').'</label></p>
+			<p class="recognition_wall"><label><input type="checkbox" id="recognize" name="recognize" value="1" checked="checked" /> '.__('Put my Donation on the Recognition Wall','dplus').'</label></p>
 			<div id="wallinfo">
 			<p class="show_onwall" id="wallops"><label for="show_onwall">'.__('Show on Wall', 'dplus').':</label><br /><select name="item_number">
 				<option value="0:'.$user_ID.'">'.__('Do not show any information','dplus').'</option>
@@ -627,7 +627,7 @@ $output .= '</select> x
 			<p class="donor_email"><label for="donor_email">'.__('Email', 'dplus').':</label><br /><input type="text" name="os0" id="donor_email" /></p>
 			<p class="donor_url"><label for="donor_url">'.__('Website', 'dplus').':</label><br /><input type="text" name="on1" id="donor_url" /></p>
 			<p  class="donor_comment"><label for="donor_comment">'.__('Comments', 'dplus').':</label><br /><textarea name="os1" id="donor_comment" rows="4" cols="45" style="width:90%"></textarea><br /><span id="charinfo">'.__('Write your comment within 199 characters.','dplus').'</span> </p></div>';
-			//endif;
+			endif;
 
 			$output .= '
 <input type="hidden" name="notify_url" value="'.$notify.'">
