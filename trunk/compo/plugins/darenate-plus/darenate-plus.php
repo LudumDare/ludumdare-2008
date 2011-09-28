b05c4d7c8cd34420aab24a188223947835004460
mikekasprzak <mikekasprzak@a6a53419-064e-0410-8e23-07da878cd106>
2011-09-28 20:55:31 +0000

    fun

--- trunk/compo/plugins/darenate-plus/darenate-plus.php
+++ trunk/compo/plugins/darenate-plus/darenate-plus.php
@@ -591,8 +591,8 @@ if( !class_exists('DarenatePlus') ):
 			$output = '<form id="darenateplusform" action="'.$verifyurlz[$dplus['testing_mode']].'" method="post">';
 
 			$output .= '
-			<p class="donor_name"><label for="donor_name"><strong>'.__('Name', 'dplus').'</strong>:</label><br /><input type="text" name="on0" id="donor_name" /><br /><small>(leave blank if you want to be Anonymous)</small></p>
-			<p class="donor_email"><label for="donor_email"><strong>'.__('Email', 'dplus').'</strong>:</label><br /><input type="text" name="os0" id="donor_email" /><br /><small>(For reference only)</small></p>
+			<p class="donor_name"><label for="donor_name"><strong>'.__('Name', 'dplus').'</strong>:</label><br /><input type="text" name="on0" id="donor_name" /><br /><small>Leave blank if you want to be Anonymous</small></p>
+			<p class="donor_email"><label for="donor_email"><strong>'.__('Email', 'dplus').'</strong>:</label><br /><input type="text" name="os0" id="donor_email" /><br /><small>For reference only</small></p>
 			<p class="donor_url"><label for="donor_url"><strong>'.__('Website', 'dplus').'</strong>:</label><br /><input type="text" name="on1" id="donor_url" /></p>
 			';
 
