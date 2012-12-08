43cd6a8c6f9c34b1bd3952d883a2bf1ab46e0ae6
philhassey@gmail.com <philhassey@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2012-12-08 18:52:21 +0000

    goods

--- trunk/compo/plugins/compo2/preview.php
+++ trunk/compo/plugins/compo2/preview.php
@@ -264,11 +264,13 @@ function _compo2_preview_show($params,$uid,$comments=true) {
     }
     echo "</table>";
     
+    echo "<p>";
     if ($params["jcat"]) {
         $link = get_bloginfo("url")."/?category_name={$params["jcat"]}&author_name={$user["user_nicename"]}";
-        echo "<p><a href='$link' target='_blank'>View {$user["display_name"]}'s journal.</a></p>";
+        echo "<a href='$link' target='_blank'>View {$user["display_name"]}'s journal.</a> | ";
     }
-    echo "<p><a href=\"../author/{$user["user_nicename"]}/\">View all entries by {$user["display_name"]}</a></p>";
+    echo "<a href=\"../author/{$user["user_nicename"]}/\">View all entries by {$user["display_name"]}</a>";
+    echo "</p>";
     
     if ($params["state"] == "results" || $params["state"] == "admin") {
         _compo2_results_ratings($params,$uid);
