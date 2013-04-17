a5850d0e281e11b81abc570825b2e8a7d8f040de
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2013-04-17 04:19:15 +0000

    new data, sorry

--- trunk/compo/plugins/twidget/twidget.php
+++ trunk/compo/plugins/twidget/twidget.php
@@ -21,32 +21,53 @@ class Twidget extends WP_Widget {
 	function widget($args, $instance) {
 		extract($args);
 		
-		$title = apply_filters( 'widget_title', $instance['title'] );
+		//$title = apply_filters( 'widget_title', $instance['title'] );
 
 		echo $before_widget;
-		if ( ! empty( $title ) )
-			echo $before_title . $title . $after_title;
-		echo __( 'Hello, World!', 'text_domain' );
+//		if ( ! empty( $title ) )
+//			echo $before_title . $title . $after_title;
+//		echo __( 'Hello, World!', 'text_domain' );
 		echo $after_widget;
 	}
 		
 	function update($new_instance, $old_instance) {
 		$instance = $old_instance;
-		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['apikey'] = strip_tags( $new_instance['apikey'] );
+		$instance['game'] = strip_tags( $new_instance['apikey'] );
+		$instance['faqurl'] = strip_tags( $new_instance['faqurl'] );
 		return $instance;
 	}
 
 	function form($instance) {
-		if ( isset( $instance[ 'title' ] ) ) {
-			$title = $instance[ 'title' ];
+		if ( isset( $instance[ 'apikey' ] ) ) {
+			$apikey = $instance[ 'apikey' ];
 		}
 		else {
-			$title = __( 'New title', 'text_domain' );
+			$apikey = __( '', 'text_domain' );
 		}
+		
+		if ( isset( $instance[ 'game' ] ) ) {
+			$game = $instance[ 'game' ];
+		}
+		else {
+			$game = __( 'Diablo III', 'text_domain' );
+		}
+		
+		if ( isset( $instance[ 'faqurl' ] ) ) {
+			$faqurl = $instance[ 'faqurl' ];
+		}
+		else {
+			$faqurl = __( 'streaming-faq/', 'text_domain' );
+		}
+		
 		?>
 		<p>
-		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		<label for="<?php echo $this->get_field_id( 'apikey' ); ?>"><?php _e( 'Twitch API Key:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'apikey' ); ?>" name="<?php echo $this->get_field_name( 'apikey' ); ?>" type="text" value="<?php echo esc_attr( $apikey ); ?>" />
+		<label for="<?php echo $this->get_field_id( 'game' ); ?>"><?php _e( 'Game Name:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'game' ); ?>" name="<?php echo $this->get_field_name( 'game' ); ?>" type="text" value="<?php echo esc_attr( $game ); ?>" />
+		<label for="<?php echo $this->get_field_id( 'faqurl' ); ?>"><?php _e( 'FAQ URL:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'faqurl' ); ?>" name="<?php echo $this->get_field_name( 'faqurl' ); ?>" type="text" value="<?php echo esc_attr( $faqurl ); ?>" />
 		</p>
 		<?php 
 	}
