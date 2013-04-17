40e53eee71fe690c199c4d37859b737c561bee27
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2013-04-17 07:35:21 +0000

    forgot the object type.

--- trunk/compo/plugins/twidget/src/twidget.js
+++ trunk/compo/plugins/twidget/src/twidget.js
@@ -40,7 +40,7 @@ function ShowTwitchTVVideo( AutoStart ) {
 		MyText += GetTwitchTVPlayer( Stream.channel.name, 282, 188, AutoStart, 25 );
 	}
 	else {
-		MyText += '<div style="vertical-align:middle;display:table-cell;width:282px;height:188px;"><span class="Intense">Live Streaming Video!</span><br />Click on a video to start playing<br /><br /><span style="font-size:20px;">To stream here, set your<br /><object data="'+TwitchTV_BaseDir+'TwitchLogo.svg" height="30" style="vertical-align:middle;"></object> game to <span class="Standout">' + TwitchTV_Game + '</span></span></div>';
+		MyText += '<div style="vertical-align:middle;display:table-cell;width:282px;height:188px;"><span class="Intense">Live Streaming Video!</span><br />Click on a video to start playing<br /><br /><span style="font-size:20px;">To stream here, set your<br /><object data="'+TwitchTV_BaseDir+'TwitchLogo.svg" height="30" style="vertical-align:middle;" type="image/svg+xml"></object> game to <span class="Standout">' + TwitchTV_Game + '</span></span></div>';
 	}
 
 	var Out = $("#TTV_Video");
