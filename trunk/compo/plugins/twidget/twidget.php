40b0aee21b9c6c91c8df4f25c6370fb73af1a544
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2013-04-17 04:10:39 +0000

    twidget again.

--- trunk/compo/plugins/twidget/twidget.php
+++ trunk/compo/plugins/twidget/twidget.php
@@ -11,44 +11,47 @@ License: BSD
 
 class Twidget extends WP_Widget {
 	function __construct() {
-		
-	}
-	
-//	function RandomPostWidget() {
-//		$widget_ops = array('classname' => 'Twidget', 'description' => 'Twitch.tv widget' );
-//		$this->WP_Widget('Twidget', 'Twitch.tv widget', $widget_ops);
-//	}
-//	
-	function form($instance) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
-		$title = $instance['title'];
-		?>
-		<p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
-		<?php
-	}
-	
-	function update($new_instance, $old_instance) {
-		$instance = $old_instance;
-		$instance['title'] = $new_instance['title'];
-		return $instance;
+		parent::__construct(
+			'twidget', // Base ID //
+			'Twidget', // Name //
+			array( 'description' => __( 'Twitch.tv widget', 'text_domain' ), ) // ARGS //
+		);
 	}
 	
 	function widget($args, $instance) {
-		extract($args, EXTR_SKIP);
+		extract($args);
 		
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
 		echo $before_widget;
-		$title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
-		
-		if (!empty($title))
-		  echo $before_title . $title . $after_title;;
-		
-		// WIDGET CODE GOES HERE
-		echo "<h1>This is my new widget!</h1>";
-		
+		if ( ! empty( $title ) )
+			echo $before_title . $title . $after_title;
+		echo __( 'Hello, World!', 'text_domain' );
 		echo $after_widget;
+	}
+		
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		return $instance;
+	}
+
+	function form($instance) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( 'New title', 'text_domain' );
+		}
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<?php 
 	}
 }
 
-add_action( 'widgets_init', create_function('', 'return register_widget("Twidget");') );?>
+add_action( 'widgets_init', function(){register_widget("Twidget");} );
 
 ?>
