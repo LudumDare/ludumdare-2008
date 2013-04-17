3a68edb635d81d54ff67b8cc0d6fc6b90ca19630
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2013-04-17 06:01:21 +0000

    updated the minified file.

--- trunk/compo/plugins/twidget/src/make.bat
+++ trunk/compo/plugins/twidget/src/make.bat
@@ -1,2 +1,2 @@
-uglifyjs twidget.js -c -m -o ../twidget.min.js
-cleancss twidget.css -e --s0 -o ../twidget.min.css
+call uglifyjs twidget.js -c -m -o ../twidget.min.js
+call cleancss twidget.css -e --s0 -o ../twidget.min.css
