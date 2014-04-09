e3886fd958280c4844638b0b2825581754be9ddb
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-04-09 19:26:23 +0000

    header

--- trunk/compo/themes/ludum/header.php
+++ trunk/compo/themes/ludum/header.php
@@ -59,8 +59,9 @@ if ( !$withcomments && !is_single() ) {
 
 <div id="header">
 	<div id="headerimg">
-		<h1 style="text-align:left; font-size:70px"><a href="<?php echo get_option('home'); ?>/"><?php /* bloginfo('name'); */ echo str_repeat("&nbsp;",30); ?></a></h1><br />
-		<span style="text-align:center;"><a href="http://www.ludumdare.com/"><strong>Home</strong></a> | <a href="http://www.ludumdare.com/compo/rules/"><strong>Rules and Guide</strong></a> | <a href="http://www.ludumdare.com/compo/wp-login.php"><strong>Sign In/Create Account</strong></a> | <a href="http://www.ludumdare.com/compo/wp-admin/post-new.php"><strong>Write a Post</strong></a> | <a href="http://www.reddit.com/r/ludumdare/"><img src="http://www.ludumdare.com/compo/wp-content/uploads/2011/11/reddit.ico" width="16"> <strong>Reddit</strong></a> | <strong>#ludumdare</strong> on <strong>irc.afternet.org (<a href="http://www.ludumdare.com/compo/irc/">Info</a>)</strong></span>
+		<a href="<?php echo get_option('home'); ?>/"><img src="wp-content/themes/ludum/images/blank.gif" width="920" height="104" /></div></a></br />
+		<!--<h1 style="text-align:left; font-size:70px"><a href="<?php echo get_option('home'); ?>/"><?php /* bloginfo('name'); */ echo str_repeat("&nbsp;",30); ?></a></h1><br />-->
+		<center><a href="http://www.ludumdare.com/"><strong>Home</strong></a> | <a href="http://www.ludumdare.com/compo/rules/"><strong>Rules and Guide</strong></a> | <a href="http://www.ludumdare.com/compo/wp-login.php"><strong>Sign In/Create Account</strong></a> | <a href="http://www.ludumdare.com/compo/wp-admin/post-new.php"><strong>Write a Post</strong></a> | <a href="http://www.reddit.com/r/ludumdare/"><img src="http://www.ludumdare.com/compo/wp-content/uploads/2011/11/reddit.ico" width="16"> <strong>Reddit</strong></a> | <strong>#ludumdare</strong> on <strong>irc.afternet.org (<a href="http://www.ludumdare.com/compo/irc/">Info</a>)</strong></center>
 		<!--<div class="description"><?php bloginfo('description'); ?></div>-->
 	</div>
 </div>
