a766b64091aa1427e0b16f658fc010609861dade
mikekasprzak <mikekasprzak@gmail.com>
2014-04-30 16:32:27 +0000

    theme adjustments for moderation

--- compo/plugins/post-mod-dare/post-mod-dare.php
+++ compo/plugins/post-mod-dare/post-mod-dare.php
@@ -10,7 +10,7 @@ Description: Inlined Pending Post Moderation (Spammers have driven us to this).
 
 /* http://wordpress.stackexchange.com/questions/103938/how-to-display-pending-posts-on-the-homepage-only-for-editors */
 function allow_pending_posts_wpse_103938($qry) {
-  if (!is_admin() && current_user_can('edit_posts')) {
+  if (!is_admin() && current_user_can('edit_others_posts')) {
     $qry->set('post_status', array('publish','pending'));
   }
 }
