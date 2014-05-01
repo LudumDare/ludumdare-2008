a3e4242c939615ef1e1452c580d11797771f97c5
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-05-01 16:02:35 +0000

    We should be able to promote and quarantine users now

--- trunk/compo/plugins/post-mod-dare/post-mod-dare.php
+++ trunk/compo/plugins/post-mod-dare/post-mod-dare.php
@@ -10,8 +10,7 @@ Description: Inlined Pending Post Moderation (Spammers have driven us to this).
 
 require_once( ABSPATH . "wp-includes/pluggable.php" );
 
-//function to print publish button
-function show_publish_button(){
+function show_publish_buttons(){
 	Global $post;
 	//only print fi admin
 	if (current_user_can('edit_others_posts')){
@@ -26,14 +25,32 @@ function show_publish_button(){
 		<input id="submit" type="submit" name="submit" value="Remove" /></form>';
 	}
 }
+
+function show_promote_buttons(){
+	Global $post;
+	//only print fi admin
+	if (current_user_can('edit_others_posts')){
+		echo '
+		<form action="" method="POST" name="front_end_promote"><input id="pid" type="hidden" name="pid" value="'.$post->ID.'" />
+		<input id="FE_USER_PROMOTE" type="hidden" name="FE_USER_PROMOTE" value="FE_USER_PROMOTE" />
+		<input id="submit" type="submit" name="submit" value="Promote to Author" /></form>';
+	}
+}
+
 	
-//function to update post status
+// Update the post status
 function change_post_status($post_id,$status){
 	$current_post = get_post( $post_id, 'ARRAY_A' );
 	$current_post['post_status'] = $status;
 	wp_update_post($current_post);
 }
-	
+// Update the user level //
+function change_user_level($user_id,$status){
+	$newuser = new WP_User( $user_id /*$user->ID*/ );
+	$newuser->set_role( $status );
+}
+
+// Responses to Post Status Changes //	
 if (isset($_POST['FE_PUBLISH']) && $_POST['FE_PUBLISH'] == 'FE_PUBLISH'){
 	if (isset($_POST['pid']) && !empty($_POST['pid'])){
 		change_post_status((int)$_POST['pid'],'publish');
@@ -45,8 +62,29 @@ if (isset($_POST['FE_TRASH']) && $_POST['FE_TRASH'] == 'FE_TRASH'){
 	}
 }
 
+// Responses to User Level Changes //
+if (isset($_POST['FE_USER_PROMOTE']) && $_POST['FE_USER_PROMOTE'] == 'FE_USER_PROMOTE'){
+	if (isset($_POST['pid']) && !empty($_POST['pid'])) {
+		$current_post = get_post( (int)$_POST['pid'], 'ARRAY_A' );		
+		change_user_level( $current_post['post_author'], 'author' );
+	}
+}
+if (isset($_POST['FE_USER_DEMOTE']) && $_POST['FE_USER_DEMOTE'] == 'FE_USER_DEMOTE'){
+	if (isset($_POST['pid']) && !empty($_POST['pid'])) {
+		$current_post = get_post( (int)$_POST['pid'], 'ARRAY_A' );		
+		change_user_level( $current_post['post_author'], 'subscriber' );
+	}
+}
+if (isset($_POST['FE_USER_RESET']) && $_POST['FE_USER_RESET'] == 'FE_USER_RESET'){
+	if (isset($_POST['pid']) && !empty($_POST['pid'])) {
+		$current_post = get_post( (int)$_POST['pid'], 'ARRAY_A' );		
+		change_user_level( $current_post['post_author'], 'contributor' );
+	}
+}
+
 
 /* http://wordpress.stackexchange.com/questions/103938/how-to-display-pending-posts-on-the-homepage-only-for-editors */
+// If the user is of high enough level, modify the query to return both pending and published posts // 
 function allow_pending_posts_wpse_103938($qry) {
   if (!is_admin() && current_user_can('edit_others_posts')) {
     $qry->set('post_status', array('publish','pending'));
