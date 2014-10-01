10c84f92216c9e715af928064816e71797e90e65
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-10-01 20:05:47 +0000

    added shim for csv

--- trunk/compo/plugins/darenate-plus/patreon.php
+++ trunk/compo/plugins/darenate-plus/patreon.php
@@ -1,8 +1,7 @@
 <?PHP
 
 
-# Better response code PHP Shim, from here: http://stackoverflow.com/a/12018482
-// For 4.3.0 <= PHP <= 5.4.0
+# Shim for PHP <= 5.4.0, from here: http://stackoverflow.com/a/12018482
 if (!function_exists('http_response_code')) {
 	function http_response_code($newcode = NULL) {
 		static $code = 200;
@@ -15,6 +14,66 @@ if (!function_exists('http_response_code')) {
 	}
 }
 # PHP Shim End #
+
+# Shim for PHP < 5.3, from http://php.net/manual/en/function.str-getcsv.php#98088
+if (!function_exists('str_getcsv')) { 
+    function str_getcsv($input, $delimiter = ',', $enclosure = '"', $escape = '\\', $eol = '\n') { 
+        if (is_string($input) && !empty($input)) { 
+            $output = array(); 
+            $tmp    = preg_split("/".$eol."/",$input); 
+            if (is_array($tmp) && !empty($tmp)) { 
+                while (list($line_num, $line) = each($tmp)) { 
+                    if (preg_match("/".$escape.$enclosure."/",$line)) { 
+                        while ($strlen = strlen($line)) { 
+                            $pos_delimiter       = strpos($line,$delimiter); 
+                            $pos_enclosure_start = strpos($line,$enclosure); 
+                            if ( 
+                                is_int($pos_delimiter) && is_int($pos_enclosure_start) 
+                                && ($pos_enclosure_start < $pos_delimiter) 
+                                ) { 
+                                $enclosed_str = substr($line,1); 
+                                $pos_enclosure_end = strpos($enclosed_str,$enclosure); 
+                                $enclosed_str = substr($enclosed_str,0,$pos_enclosure_end); 
+                                $output[$line_num][] = $enclosed_str; 
+                                $offset = $pos_enclosure_end+3; 
+                            } else { 
+                                if (empty($pos_delimiter) && empty($pos_enclosure_start)) { 
+                                    $output[$line_num][] = substr($line,0); 
+                                    $offset = strlen($line); 
+                                } else { 
+                                    $output[$line_num][] = substr($line,0,$pos_delimiter); 
+                                    $offset = ( 
+                                                !empty($pos_enclosure_start) 
+                                                && ($pos_enclosure_start < $pos_delimiter) 
+                                                ) 
+                                                ?$pos_enclosure_start 
+                                                :$pos_delimiter+1; 
+                                } 
+                            } 
+                            $line = substr($line,$offset); 
+                        } 
+                    } else { 
+                        $line = preg_split("/".$delimiter."/",$line); 
+    
+                        /* 
+                         * Validating against pesky extra line breaks creating false rows. 
+                         */ 
+                        if (is_array($line) && !empty($line[0])) { 
+                            $output[$line_num] = $line; 
+                        }  
+                    } 
+                } 
+                return $output; 
+            } else { 
+                return false; 
+            } 
+        } else { 
+            return false; 
+        } 
+    } 
+}
+# PHP Shim End #
+
 
 
 function rest_post($request) {
