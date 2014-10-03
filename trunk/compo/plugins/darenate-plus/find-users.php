f07c615444d721c33eb87753b27203a796ae5238
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-10-03 18:51:05 +0000

    populating the users...

--- trunk/compo/plugins/darenate-plus/find-users.php
+++ trunk/compo/plugins/darenate-plus/find-users.php
@@ -126,7 +126,7 @@ function rest_get($request) {
 	
 	echo "Unique: " . count($byAddress) . "\n";
 	$byAddress_count = count($byAddress);
-	$byAddress_values = array_values($byAddress);
+	$byAddress_values = array_keys($byAddress);
 	for ($idx = 0; $idx < $byAddress_count; $idx++ ) {
 		echo $byAddress_values[$idx] . " : " . $byAddress[$idx]["id"] . " = " . $byAddress[$idx]["total"] . "\n";
 	}
