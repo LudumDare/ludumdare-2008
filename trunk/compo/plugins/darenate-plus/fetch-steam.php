08a4c9e39aee9ea4d6d29f74d2cdb1dbe1b81ee2
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-10-05 20:20:42 +0000

    steam fetch test

--- trunk/compo/plugins/darenate-plus/fetch-steam.php
+++ trunk/compo/plugins/darenate-plus/fetch-steam.php
@@ -1,7 +1,28 @@
 <?php
 
-$url = "http://steamcommunity.com/groups/ludum/memberslistxml/?xml=1";
-$xml = simplexml_load_file($url);
-print_r($xml);
+echo "XML Grab\n";
+{
+	$url = "http://steamcommunity.com/groups/ludum/memberslistxml/?xml=1";
+	$xml = simplexml_load_file($url);
+	print_r($xml);
+}
+
+require "simple_html_dom.php"
+
+echo "HTML Grab\n";
+{
+	$html = file_get_html( "http://store.steampowered.com/curator/537829/" );
+	
+	print_r( $html->find('div.num_followers') );
+}
+
+//echo "HTTP Grab\n";
+//{
+//	$response = http_get( "http://store.steampowered.com/curator/537829/", array("timeout"=>2), $info );
+//	
+//	preg_match('/<div >(.*?)<\/div>/s', $response, $matches);
+//	
+//	print_r($matches);
+//}
 
 ?>
