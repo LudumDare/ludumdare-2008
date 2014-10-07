5aabb3df7ec7a9a498476860028dd1a6542048c2
mikekasprzak <mikekasprzak@gmail.com>
2014-10-07 22:04:39 +0000

    pushing cron

--- compo/plugins/steam-widget/cron.php
+++ compo/plugins/steam-widget/cron.php
@@ -1,12 +1,12 @@
 #!/usr/bin/php
 <?php
 
-if (php_sapi_name() == "cli") {
-	echo "Greetings\n";
-}
-else {
+if (php_sapi_name() !== "cli") {
 	// Jurassic Park //
 	echo "\n\nClever girl.\n";
+	exit(1);
 }
+
+echo "Greetings\n";
 
 ?>
