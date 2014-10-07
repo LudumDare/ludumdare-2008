8162cebb0012ed4954394e46703b633922ea509a
mikekasprzak <mikekasprzak@gmail.com>
2014-10-07 22:06:06 +0000

    pushing cron

--- compo/plugins/steam-widget/cron.php
+++ compo/plugins/steam-widget/cron.php
@@ -1,12 +1,13 @@
 #!/usr/bin/php
 <?php
 
+// Only allow script to execute if via PHP-CLI (i.e. Cron Job) //
 if (php_sapi_name() !== "cli") {
 	// Jurassic Park //
-	echo "\n\nClever girl.\n";
+	echo "Clever girl.\n";
 	exit(1);
 }
 
-echo "Greetings\n";
+echo "Greetings!\n";
 
 ?>
