9f69903de485e21a095c90f267248c41b7aaac91
mikekasprzak <mikekasprzak@gmail.com>
2014-10-08 06:44:55 +0000

    now pulling steam data

--- compo/plugins/steam-widget/fetch-steam.php
+++ compo/plugins/steam-widget/fetch-steam.php
@@ -41,24 +41,38 @@ require "simple_html_dom.php";
 
 // NO API, so an HTTP Get //
 function steam_curator_get( $curator_id ) {
-	$html = file_get_html( "http://store.steampowered.com/curator/". $curator_id ."/" );
+	// Get the main HTML file for Avatar and Num Followers //
+	// http://store.steampowered.com/curator/537829/
+	$main_url = "http://store.steampowered.com/curator/". $curator_id ."/";
+	$main_html = file_get_html( $main_url );
 	
 	$ret = array();
-	$ret['followers'] = $html->find('.num_followers', 0)->plaintext;
-	$ret['avatar'] = $html->find('.curator_avatar', 0)->src;
+	$ret['followers'] = $main_html->find('.num_followers', 0)->plaintext;
+	$ret['avatar'] = $main_html->find('.curator_avatar', 0)->src;
+
+	// Use AJAX
+	// http://store.steampowered.com/curators/ajaxgetcuratorrecommendations/537829//?query=&start=0&count=20
+	$game_url = "http://store.steampowered.com/curators/ajaxgetcuratorrecommendations/". $curator_id ."//?query=&start=0&count=20";
+	$game_json = json_decode(file_get_contents($game_url));
+	print_r( $game_json );
+
+	//$game_html = file_get_html( "http://store.steampowered.com/curators/ajaxgetcuratorrecommendations/". $curator_id ."//?query=&start=0&count=20" );
+
+/*	
+	// http://store.steampowered.com/apphoverpublic/201040?l=english&pagev6=true
 	
 	$ret['games'] = array();
-	foreach( $html->find('.recommendation') as $elm ) {
+	foreach( $game_html->find('.recommendation') as $elm ) {
 		//$ret['games'][] = $elm->attr['data-ds-appid'];
 		$ret['games'][] = Array(
 			'appid' => $elm->attr['data-ds-appid'],
 			'banner' => $elm->find('.recommendation_app_small_cap',0)->src,
 			'url' => $elm->find('a',0)->href,
-			'desc' => $elm->find('.recommendation_desc',0)->plaintext,
+			'desc' => trim($elm->find('.recommendation_desc',0)->plaintext),
 			'read_url' => $elm->find('.recommendation_readmore',0)->find('a',0)->href
 		);
 	}
-	
+*/	
 	return $ret;
 }
 
