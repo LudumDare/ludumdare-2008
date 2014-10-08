7080caee48dd2f24ad74b8d7d79996d66e1d7f67
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-10-08 18:44:12 +0000

    seems it's postmoddares fault

--- trunk/compo/plugins/post-mod-dare/post-mod-dare.php
+++ trunk/compo/plugins/post-mod-dare/post-mod-dare.php
@@ -144,7 +144,7 @@ add_filter('login_message', 'custom_login_message');
 function allow_pending_posts_wpse_103938($query) {
 	$post_status = $query->get('post_status');
 	$_status = ($post_status == 'publish');
-	echo "[" . $post_status . "](".$_status . ') ' . gettype($_status) . "*";
+	//echo "[" . $post_status . "](".$_status . ') ' . gettype($_status) . "*";
 	if (!is_admin() && current_user_can('edit_others_posts')) {
 		//$po_status = $query->get('post_status');
 		//if ( $po_status === 'publish' ) {
