e2f534c11bb25e8238b0c59c358c2a9f3a63ac68
mikekasprzak <mikekasprzak@gmail.com>
2014-10-13 21:31:07 +0000

    widget time

--- compo/plugins/steam-widget/steam-widget.php
+++ compo/plugins/steam-widget/steam-widget.php
@@ -33,62 +33,101 @@ function wp_steam_games_get( $more_query = "" ) {
 }
 
 
-/*
-<div class="header"></div>
-<div class="content nobottom">
-  <div class="logo"></div>
-  <div class="avatar"></div>
-  <div class="headline_small">STEAM CURATOR</div>
-  <div class="headline_big">Ludum Dare</div>
-</div>
-<div class="content nobottom">Steam games created during Ludum Dare events. <strong>Follow us!</strong> Help share LD with everyone!</div>
-<div class="content overflow">
-  <div class="left"><div class="countbox" style="color:#8bc53f">
-    <div class="count">13</div>
-    <div class="label">GAMES</div>
-  </div></div>
-  <div class="right"><div class="countbox" style="color:#62a7e3">
-  <span class="right">
-    <div class="follow_button" style="margin-left:12px;margin-top:5px">Follow</div>
-  </span>
-  <span class="left">
-    <div class="count">2,000</div>
-    <div class="label">FOLLOWERS</div>
-  </span>
-  </div></div>
-</div>
-<div class="rule"></div>
-<div class="content center">
-  <div class="banner new"><a href="somehting" title="open on STEAM"><img src="http://cdn.akamai.steamstatic.com/steam/apps/274290/capsule_231x87.jpg" /></a></div>
-  <div class="banner soon"><a href="somehting" title="open on STEAM"><img src="http://cdn.akamai.steamstatic.com/steam/apps/321560/capsule_231x87.jpg" /></a></div>
-  <div class="banner"><a href="somehting" title="open on STEAM"><img src="http://cdn.akamai.steamstatic.com/steam/apps/274190/capsule_231x87.jpg" /></a></div>
-</div>
-<div class="rule"></div>
-<div class="content nobottom">
-  <span class="right">
-    <div class="follow_button" style="float:right">Join</div>
-  </span>
-  <div class="avatar_small"></div>
-  <div class="headline_small">STEAM GROUP</div>
-  <div class="headline_big">Ludum Dare</div>
-</div>
-<div class="content nobottom">Dev together. Play together.</div>
-<div class="content overflow">
-  <div class="left"><div class="countbox" style="color:#9a9a9a">
-    <div class="count">555</div>
-    <div class="label">MEMBERS</div>
-  </div></div>
-  <div class="right"><div class="countbox" style="color:#62a7e3">
-    <div class="count">55</div>
-    <div class="label">ONLINE</div>
-  </div></div>
-  <div class="center"><div class="countbox" style="color:#8bc53f">
-    <div class="count">5</div>
-    <div class="label">IN-GAME</div>
-  </div></div>
-</div>
-<div class="footer"></div>
-*/
+
+class SteamWidget extends WP_Widget {
+	public function __construct() {
+		parent::__construct(
+			'steam-widget', // Base ID //
+			'Steam Widget', // Name //
+			array( 'description' => __( 'Steam Group and Curator Widget', 'text_domain' ), ) // ARGS //
+		);
+	}
+
+	public function widget( $args, $instance ) {
+		$steam_info = wp_steam_info_get();
+		$steam_games = wp_steam_games_get( "ORDER BY RAND() LIMIT 3" );
+		
+		echo '
+			<div class="steambox">
+				<div class="header"></div>
+				<div class="content nobottom">
+					<div class="logo"></div>
+					<div class="avatar"></div>
+					<div class="headline_small">STEAM CURATOR</div>
+					<div class="headline_big">Ludum Dare</div>
+				</div>
+				<div class="content nobottom">Steam games created during Ludum Dare events. <strong>Follow us!</strong> Help share LD with everyone!</div>
+				<div class="content overflow">
+					<div class="left"><div class="countbox" style="color:#8bc53f">
+						<div class="count">13</div>
+						<div class="label">GAMES</div>
+					</div></div>
+					<div class="right"><div class="countbox" style="color:#62a7e3">
+						<span class="right">
+							<div class="follow_button" style="margin-left:12px;margin-top:5px">Follow</div>
+						</span>
+						<span class="left">
+							<div class="count">2,000</div>
+							<div class="label">FOLLOWERS</div>
+						</span>
+					</div></div>
+				</div>
+				<div class="rule"></div>
+				<div class="content center">
+					<div class="banner new"><a href="somehting" title="open on STEAM"><img src="http://cdn.akamai.steamstatic.com/steam/apps/274290/capsule_231x87.jpg" /></a></div>
+					<div class="banner soon"><a href="somehting" title="open on STEAM"><img src="http://cdn.akamai.steamstatic.com/steam/apps/321560/capsule_231x87.jpg" /></a></div>
+					<div class="banner"><a href="somehting" title="open on STEAM"><img src="http://cdn.akamai.steamstatic.com/steam/apps/274190/capsule_231x87.jpg" /></a></div>
+				</div>
+				<div class="rule"></div>
+				<div class="content nobottom">
+					<span class="right">
+						<div class="follow_button" style="float:right">Join</div>
+					</span>
+					<div class="avatar_small"></div>
+					<div class="headline_small">STEAM GROUP</div>
+					<div class="headline_big">Ludum Dare</div>
+				</div>
+				<div class="content nobottom">Dev together. Play together.</div>
+				<div class="content overflow">
+					<div class="left"><div class="countbox" style="color:#9a9a9a">
+						<div class="count">555</div>
+						<div class="label">MEMBERS</div>
+					</div></div>
+					<div class="right"><div class="countbox" style="color:#62a7e3">
+						<div class="count">55</div>
+						<div class="label">ONLINE</div>
+					</div></div>
+					<div class="center"><div class="countbox" style="color:#8bc53f">
+						<div class="count">5</div>
+						<div class="label">IN-GAME</div>
+					</div></div>
+				</div>
+				<div class="footer"></div>
+			</div>
+		';
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		// outputs the options form on admin
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+	}
+}
+
+
 
 add_action( 'wp_enqueue_scripts', 'steam_add_my_stylesheet' );
 function steam_add_my_stylesheet() {
