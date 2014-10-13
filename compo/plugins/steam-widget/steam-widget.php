3acd0cdbefc76c7b6be0a5b0f2d7b567fd5020d2
mikekasprzak <mikekasprzak@gmail.com>
2014-10-13 21:34:33 +0000

    widget time

--- compo/plugins/steam-widget/steam-widget.php
+++ compo/plugins/steam-widget/steam-widget.php
@@ -44,6 +44,8 @@ class SteamWidget extends WP_Widget {
 		$steam_info = wp_steam_info_get();
 		$steam_games = wp_steam_games_get( "ORDER BY RAND() LIMIT 3" );
 		
+		print_r($steam_info);
+		
 		echo '
 			<div class="steambox">
 				<div class="header"></div>
