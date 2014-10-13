915ac7c3d08ee6cc3619e15b62ff6c22f7b5c0ac
mikekasprzak <mikekasprzak@gmail.com>
2014-10-13 21:43:13 +0000

    widget time

--- compo/plugins/steam-widget/steam-widget.php
+++ compo/plugins/steam-widget/steam-widget.php
@@ -52,64 +52,64 @@ class SteamWidget extends WP_Widget {
 		$steam_info = wp_steam_info_get();
 		$steam_games = wp_steam_games_get( "ORDER BY RAND() LIMIT 3" );
 		
-		echo '
-			<div class="steambox">
-				<div class="header"></div>
-				<div class="content nobottom">
-					<div class="logo"></div>
-					<div class="avatar"></div>
-					<div class="headline_small">STEAM CURATOR</div>
-					<div class="headline_big">Ludum Dare</div>
+		echo "
+			<div class='steambox'>
+				<div class='header'></div>
+				<div class='content nobottom'>
+					<div class='logo'></div>
+					<div class='avatar'></div>
+					<div class='headline_small'>STEAM CURATOR</div>
+					<div class='headline_big'>Ludum Dare</div>
 				</div>
-				<div class="content nobottom">Steam games created during Ludum Dare events. <strong>Follow us!</strong> Help share LD with everyone!</div>
-				<div class="content overflow">
-					<div class="left"><div class="countbox" style="color:#8bc53f">
-						<div class="count">{$steam_info["curator_games"]}</div>
-						<div class="label">GAMES</div>
+				<div class='content nobottom'>Steam games created during Ludum Dare events. <strong>Follow us!</strong> Help share LD with everyone!</div>
+				<div class='content overflow'>
+					<div class='left'><div class='countbox' style='color:#8bc53f'>
+						<div class='count'>{$steam_info['curator_games']}</div>
+						<div class='label'>GAMES</div>
 					</div></div>
-					<div class="right"><div class="countbox" style="color:#62a7e3">
-						<span class="right">
-							<div class="follow_button" style="margin-left:12px;margin-top:5px">Follow</div>
+					<div class='right'><div class='countbox' style='color:#62a7e3'>
+						<span class='right'>
+							<div class='follow_button' style='margin-left:12px;margin-top:5px'>Follow</div>
 						</span>
-						<span class="left">
-							<div class="count">{$steam_info["curator_followers"]}</div>
-							<div class="label">FOLLOWERS</div>
+						<span class='left'>
+							<div class='count'>{$steam_info['curator_followers']}</div>
+							<div class='label'>FOLLOWERS</div>
 						</span>
 					</div></div>
 				</div>
-				<div class="rule"></div>
-				<div class="content center">
-					<div class="banner new"><a href="somehting" title="open on STEAM"><img src="http://cdn.akamai.steamstatic.com/steam/apps/274290/capsule_231x87.jpg" /></a></div>
-					<div class="banner soon"><a href="somehting" title="open on STEAM"><img src="http://cdn.akamai.steamstatic.com/steam/apps/321560/capsule_231x87.jpg" /></a></div>
-					<div class="banner"><a href="somehting" title="open on STEAM"><img src="http://cdn.akamai.steamstatic.com/steam/apps/274190/capsule_231x87.jpg" /></a></div>
+				<div class='rule'></div>
+				<div class='content center'>
+					<div class='banner new'><a href='somehting' title='open on STEAM'><img src='http://cdn.akamai.steamstatic.com/steam/apps/274290/capsule_231x87.jpg' /></a></div>
+					<div class='banner soon'><a href='somehting' title='open on STEAM'><img src='http://cdn.akamai.steamstatic.com/steam/apps/321560/capsule_231x87.jpg' /></a></div>
+					<div class='banner'><a href='somehting' title='open on STEAM'><img src='http://cdn.akamai.steamstatic.com/steam/apps/274190/capsule_231x87.jpg' /></a></div>
 				</div>
-				<div class="rule"></div>
-				<div class="content nobottom">
-					<span class="right">
-						<div class="follow_button" style="float:right">Join</div>
+				<div class='rule'></div>
+				<div class='content nobottom'>
+					<span class='right'>
+						<div class='follow_button' style='float:right'>Join</div>
 					</span>
-					<div class="avatar_small"></div>
-					<div class="headline_small">STEAM GROUP</div>
-					<div class="headline_big">Ludum Dare</div>
+					<div class='avatar_small'></div>
+					<div class='headline_small'>STEAM GROUP</div>
+					<div class='headline_big'>Ludum Dare</div>
 				</div>
-				<div class="content nobottom">Dev together. Play together.</div>
-				<div class="content overflow">
-					<div class="left"><div class="countbox" style="color:#9a9a9a">
-						<div class="count">{$steam_info["group_members"]}</div>
-						<div class="label">MEMBERS</div>
+				<div class='content nobottom'>Dev together. Play together.</div>
+				<div class='content overflow'>
+					<div class='left'><div class='countbox' style='color:#9a9a9a'>
+						<div class='count'>{$steam_info['group_members']}</div>
+						<div class='label'>MEMBERS</div>
 					</div></div>
-					<div class="right"><div class="countbox" style="color:#62a7e3">
-						<div class="count">{$steam_info["group_members_online"]}</div>
-						<div class="label">ONLINE</div>
+					<div class='right'><div class='countbox' style='color:#62a7e3'>
+						<div class='count'>{$steam_info['group_members_online']}</div>
+						<div class='label'>ONLINE</div>
 					</div></div>
-					<div class="center"><div class="countbox" style="color:#8bc53f">
-						<div class="count">{$steam_info["group_members_in_game"]}</div>
-						<div class="label">IN-GAME</div>
+					<div class='center'><div class='countbox' style='color:#8bc53f'>
+						<div class='count'>{$steam_info['group_members_in_game']}</div>
+						<div class='label'>IN-GAME</div>
 					</div></div>
 				</div>
-				<div class="footer"></div>
+				<div class='footer'></div>
 			</div>
-		';
+		";
 	}
 
 	/**
