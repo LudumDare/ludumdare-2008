2296c09feb058d99835a88b6929e6a61de9beecc
mikekasprzak <mikekasprzak@gmail.com>
2014-10-20 06:52:17 +0000

    removing stupid from the website

--- compo/themes/ludum/index.php
+++ compo/themes/ludum/index.php
@@ -16,39 +16,39 @@
 
 		<?php while (have_posts()) : the_post(); ?>
 
-		<?php if ( current_user_can('edit_others_posts') ) { ?>
-			<?php if ( get_the_author_meta('user_level') == 1 ) { ?>
-				<div class="postflag" style="background-color: #D64;">
-					<div style="float:left">NEW USER</div>
-					<div style="float:right"><?php show_promote_buttons(); ?></div>
-				</div>
-			<?php } elseif ( current_user_can('delete_users') && ($_GET["admin"] === "1") ) { ?>
-				<div class="postflag" style="background-color: #555;">
-					<div style="float:left">EXISTING USER</div>
-					<div style="float:right"><?php show_murder_buttons(); ?></div>
-				</div>
+			<?php if ( current_user_can('edit_others_posts') ) { ?>
+				<?php if ( get_the_author_meta('user_level') == 1 ) { ?>
+					<div class="postflag" style="background-color: #D64;">
+						<div style="float:left">NEW USER</div>
+						<div style="float:right"><?php show_promote_buttons(); ?></div>
+					</div>
+				<?php } elseif ( current_user_can('delete_users') && ($_GET["admin"] === "1") ) { ?>
+					<div class="postflag" style="background-color: #555;">
+						<div style="float:left">EXISTING USER</div>
+						<div style="float:right"><?php show_murder_buttons(); ?></div>
+					</div>
+				<?php } ?>
+				<?php if ( $post->post_status == 'pending' ) { ?>
+					<div class="postflag">
+						<div style="float:left">PENDING</div>
+						<div style="float:right"><?php show_publish_buttons(); ?></div>
+					</div>
+				<?php } ?>
 			<?php } ?>
-			<?php if ( $post->post_status == 'pending' ) { ?>
-				<div class="postflag">
-					<div style="float:left">PENDING</div>
-					<div style="float:right"><?php show_publish_buttons(); ?></div>
-				</div>
+			
+			<?php if ( get_the_author_meta('display_name') == 'news' ) { ?>
+				<div class="post" style="background: #f0fff0 url('/compo/wp-content/themes/ludum/povimg/News.png') no-repeat top right;" id="post-<?php the_ID(); ?>">
+			<?php } else if ( get_the_author_meta('user_level') == 10 ) { ?>
+				<div class="post" style="background: #fffff0 url('/compo/wp-content/themes/ludum/povimg/Admin.png') no-repeat top right;" id="post-<?php the_ID(); ?>">
+			<?php } else if ( is_sticky() ) { ?>
+				<div class="post" style="background: #f8f8ff url('/compo/wp-content/themes/ludum/povimg/Featured.png') no-repeat top right;" id="post-<?php the_ID(); ?>">
+			<?php } else if ( get_post_meta(get_the_ID(), '_liked', true) >= 4 ) { ?>
+				<div class="post" style="background: #fff0f0 url('/compo/wp-content/themes/ludum/povimg/Heart.png') no-repeat top right;" id="post-<?php the_ID(); ?>" >
+			<?php } else if ( get_the_author_meta('user_level') == 7 ) { ?>
+				<div class="post" style="background: #ffffff url('/compo/wp-content/themes/ludum/povimg/Moderator.png') no-repeat top right;" id="post-<?php the_ID(); ?>">
+			<?php } else { ?>
+				<div class="post" id="post-<?php the_ID(); ?>" >
 			<?php } ?>
-		<?php } ?>
-		
-		<?php if ( get_the_author_meta('display_name') == 'news' ) { ?>
-			<div class="post" style="background: #f0fff0 url('/compo/wp-content/themes/ludum/povimg/News.png') no-repeat top right;" id="post-<?php the_ID(); ?>">
-		<?php } else if ( get_the_author_meta('user_level') == 10 ) { ?>
-			<div class="post" style="background: #fffff0 url('/compo/wp-content/themes/ludum/povimg/Admin.png') no-repeat top right;" id="post-<?php the_ID(); ?>">
-		<?php } else if ( is_sticky() ) { ?>
-			<div class="post" style="background: #f8f8ff url('/compo/wp-content/themes/ludum/povimg/Featured.png') no-repeat top right;" id="post-<?php the_ID(); ?>">
-		<?php } else if ( get_post_meta(get_the_ID(), '_liked', true) >= 4 ) { ?>
-			<div class="post" style="background: #fff0f0 url('/compo/wp-content/themes/ludum/povimg/Heart.png') no-repeat top right;" id="post-<?php the_ID(); ?>" >
-		<?php } else if ( get_the_author_meta('user_level') == 7 ) { ?>
-			<div class="post" style="background: #ffffff url('/compo/wp-content/themes/ludum/povimg/Moderator.png') no-repeat top right;" id="post-<?php the_ID(); ?>">
-		<?php } else { ?>
-			<div class="post" id="post-<?php the_ID(); ?>" >
-		<?php } ?>
 				<div style="float: right;"><?php echo get_avatar(get_the_author_id(),$size='56',$default='' ); ?></div>
 				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
                 	<div>Posted by <?php the_author_posts_link(); ?><?php $aff = get_the_author_meta('affiliation', get_the_author_ID()); if (($aff != null) && ($aff != '')) { echo ' of ' . $aff; } ?><?php $twitter = get_the_author_meta('twitter', get_the_author_ID()); if (($twitter != null) && ($twitter != '')) { echo ' (twitter: <a target="_blank" href="http://twitter.com/' . $twitter . '">@' . $twitter . '</a>)'; } ?></div>
