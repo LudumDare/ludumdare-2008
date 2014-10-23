98ebb1d8a06477748654b9c975bebde49b198a4a
mikekasprzak <mikekasprzak@gmail.com>
2014-10-23 17:13:55 +0000

    event control

--- compo/themes/ludum/index.php
+++ compo/themes/ludum/index.php
@@ -2,7 +2,7 @@
 
 	<div id="content" class="narrowcolumn">
 		<div class="event">
-			<div class="navigation"><a href="/compo/october-challenge-2014/?action=edit">Edit Submission</a> | <a href="/compo/october-challenge-2014/?action=preview">View All</a></div>
+			<div class="navigation"><a href="/compo/october-challenge-2014/?action=edit">Submit (<strong>Edit</strong>)</a> | <a href="http://www.ludumdare.com/compo/2014/09/29/october-2014-resources-and-opportunities/">Resources</a> | <a href="/compo/october-challenge-2014/?action=preview">View All</a></div>
 			<div class="name">Now: <a href="/compo/2014/09/29/october-challenge-2014/"><strong>October Challenge 2014</strong></a></div>
 		</div>
 	
