e85db335660a344414cfee1d20c0f103c695fd01
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-10-23 16:01:03 +0000

    gradients

--- trunk/compo/themes/ludum/index.php
+++ trunk/compo/themes/ludum/index.php
@@ -37,15 +37,15 @@
 			<?php } ?>
 			
 			<?php if ( get_the_author_meta('display_name') == 'news' ) { ?>
-				<div class="post" style="background: #f0fff0 url('/compo/wp-content/themes/ludum/povimg/News.png') no-repeat top right;" id="post-<?php the_ID(); ?>">
+				<div class="post ld-news" id="post-<?php the_ID(); ?>">
 			<?php } else if ( get_the_author_meta('user_level') == 10 ) { ?>
-				<div class="post" style="background: #fffff0 url('/compo/wp-content/themes/ludum/povimg/Admin.png') no-repeat top right;" id="post-<?php the_ID(); ?>">
+				<div class="post ld-admin" id="post-<?php the_ID(); ?>">
 			<?php } else if ( is_sticky() ) { ?>
-				<div class="post" style="background: #f8f8ff url('/compo/wp-content/themes/ludum/povimg/Featured.png') no-repeat top right;" id="post-<?php the_ID(); ?>">
+				<div class="post ld-sticky" id="post-<?php the_ID(); ?>">
 			<?php } else if ( get_post_meta(get_the_ID(), '_liked', true) >= 4 ) { ?>
-				<div class="post" style="background: #fff0f0 url('/compo/wp-content/themes/ludum/povimg/Heart.png') no-repeat top right;" id="post-<?php the_ID(); ?>" >
+				<div class="post ld-love" id="post-<?php the_ID(); ?>" >
 			<?php } else if ( get_the_author_meta('user_level') == 7 ) { ?>
-				<div class="post" style="background: #ffffff url('/compo/wp-content/themes/ludum/povimg/Moderator.png') no-repeat top right;" id="post-<?php the_ID(); ?>">
+				<div class="post ld-mod" id="post-<?php the_ID(); ?>">
 			<?php } else { ?>
 				<div class="post" id="post-<?php the_ID(); ?>" >
 			<?php } ?>
