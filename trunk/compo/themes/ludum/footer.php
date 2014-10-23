7f2da62e8001ede45ea3ad0c4c75c2d50161b418
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-10-23 18:46:40 +0000

    theme tweaks

--- trunk/compo/themes/ludum/footer.php
+++ trunk/compo/themes/ludum/footer.php
@@ -1,17 +1,11 @@
-<hr />
-<div id="footer">
-<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
-	<p style="color: #ffffff;">
-		<?php bloginfo('name'); ?> is proudly powered by
-		<a href="http://wordpress.org/">WordPress</a>
-		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
-		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
-		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
-	</p>
+	<hr />
+	<div id="footer">
+		<p style="color: #ffffff;">
+			<p>All posts, images, and comments are owned by their creators.</p>
+			<?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds.
+		</p>
+	</div>
 </div>
-</div>
-
-<p>All posts, images, and comments are owned by their creators.</p>
 
 		<?php wp_footer(); ?>
 </body>
