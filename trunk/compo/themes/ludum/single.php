3a6fd10ffca419518b6e11a259531c59df2870d1
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-10-23 19:30:38 +0000

    theme tweaks

--- trunk/compo/themes/ludum/single.php
+++ trunk/compo/themes/ludum/single.php
@@ -23,7 +23,19 @@
 			<?php } ?>
 		<?php } ?>
 
-		<div class="post" id="post-<?php the_ID(); ?>">
+	<?php if ( get_the_author_meta('display_name') == 'news' ) { ?>
+		<div class="ld-news post" id="post-<?php the_ID(); ?>">
+	<?php } else if ( get_the_author_meta('user_level') == 10 ) { ?>
+		<div class="ld-admin post" id="post-<?php the_ID(); ?>">
+	<?php } else if ( is_sticky() ) { ?>
+		<div class="ld-sticky post" id="post-<?php the_ID(); ?>">
+	<?php } else if ( get_post_meta(get_the_ID(), '_liked', true) >= 4 ) { ?>
+		<div class="ld-love post" id="post-<?php the_ID(); ?>" >
+	<?php } else if ( get_the_author_meta('user_level') == 7 ) { ?>
+		<div class="ld-mod post" id="post-<?php the_ID(); ?>">
+	<?php } else { ?>
+		<div class="post" id="post-<?php the_ID(); ?>" >
+	<?php } ?>
 			<div style="float: right;"><?php echo get_avatar(get_the_author_id(),$size='56',$default='' ); ?></div>
 			<h2><a href="<?php echo get_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
                         <div>Posted by <?php the_author_posts_link(); ?><?php $aff = get_the_author_meta('affiliation', get_the_author_ID()); if (($aff != null) && ($aff != '')) { echo ' of ' . $aff; } ?><?php $twitter = get_the_author_meta('twitter', get_the_author_ID()); if (($twitter != null) && ($twitter != '')) { echo ' (twitter: <a target="_blank" href="http://twitter.com/' . $twitter . '">@' . $twitter . '</a>)'; } ?></div>
