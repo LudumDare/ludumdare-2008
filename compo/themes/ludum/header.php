8de63c6f2ba3e39c82b6335667044864cd72a120
mikekasprzak <mikekasprzak@gmail.com>
2014-10-24 21:14:31 +0000

    Clocking

--- compo/themes/ludum/header.php
+++ compo/themes/ludum/header.php
@@ -94,12 +94,6 @@ require_once dirname(__FILE__)."/fncs.php"; // load up our custom function goodi
 						cdPadZero(diff.getSeconds());
 				}
 			},500);
-//var date1 = new Date(2000, 0, 1,  9, 0); // 9:00 AM
-//var date2 = new Date(2000, 0, 1, 17, 0); // 5:00 PM
-//if (date2 < date1) {
-//date2.setDate(date2.getDate() + 1);
-//}
-//var diff = date2 - date1;
 		});
 	</script>
 </head>
