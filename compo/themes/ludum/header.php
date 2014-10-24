e05915b884241d7c526c39ac0f8e85bcbc1d8edd
mikekasprzak <mikekasprzak@gmail.com>
2014-10-24 18:49:31 +0000

    Clocking

--- compo/themes/ludum/header.php
+++ compo/themes/ludum/header.php
@@ -69,8 +69,8 @@ require_once dirname(__FILE__)."/fncs.php"; // load up our custom function goodi
 			cdTimer = setInterval(function(){
 				var nowClock = new Date();
 				for (var idx = 0; idx < cdClock.length; idx++ ) {
-					var dateA = nowClock;
-					var dateB = cdClock_time[idx];
+					var dateA = cdClock_time[idx];
+					var dateB = nowClock;
 					var diff = new Date( cdDateDiff(dateA,dateB) );
 					var sep = ":";
 					if ( diff.getMilliseconds() >= 500 ) {
