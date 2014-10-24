3e3bcbf5b4425b630db2003899bb26c5d39fa454
mikekasprzak <mikekasprzak@gmail.com>
2014-10-24 22:14:01 +0000

    Clocking

--- compo/themes/ludum/header.php
+++ compo/themes/ludum/header.php
@@ -43,7 +43,7 @@ require_once dirname(__FILE__)."/fncs.php"; // load up our custom function goodi
 		var cdClock;
 		var cdClock_time = [];
 		/*var cdServerClock = new Date(<?php echo gmmktime()*1000; ?>);*/
-		var cdServerClock = new Date(<?php echo $_SERVER['REQUEST_TIME']*1000; ?>);
+		var cdServerClock = new Date(<?php echo $_SERVER['REQUEST_TIME']; ?>*1000);
 		var cdLocalClock = new Date();
 		var cdTimer;
 		function cdPadZero( num ) {
