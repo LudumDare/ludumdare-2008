0a5065f2d17d1080c6fac1091265f63e2b85207e
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-10-26 20:29:29 +0000

    New neutral color scheme

--- trunk/compo/themes/ludum/single.php
+++ trunk/compo/themes/ludum/single.php
@@ -53,40 +53,40 @@
 				<?php the_tags( '<p>Tags: ', ', ', '</p>'); ?>
 				<div style="float: right;border: 1px solid #eee;padding: 2px;background: #fff;"><?php if(function_exists(getILikeThis)) getILikeThis('get'); ?></div>
 			</div>
-			<div class="footer">
-				<p class="postmetadata">
-					<small>
-						This entry was posted
-						<?php /* This is commented, because it requires a little adjusting sometimes.
-							You'll need to download this plugin, and follow the instructions:
-							http://binarybonsai.com/archives/2004/08/17/time-since-plugin/ */
-							/* $entry_datetime = abs(strtotime($post->post_date) - (60*120)); echo time_since($entry_datetime); echo ' ago'; */ ?>
-						on <?php the_time('l, F jS, Y') ?> at <?php the_time() ?>
-						and is filed under <?php the_category(', ') ?>.
-						You can follow any responses to this entry through the <?php comments_rss_link('RSS 2.0'); ?> feed.
+		</div>
+		<div class="footer">
+			<p class="postmetadata">
+				<small>
+					This entry was posted
+					<?php /* This is commented, because it requires a little adjusting sometimes.
+						You'll need to download this plugin, and follow the instructions:
+						http://binarybonsai.com/archives/2004/08/17/time-since-plugin/ */
+						/* $entry_datetime = abs(strtotime($post->post_date) - (60*120)); echo time_since($entry_datetime); echo ' ago'; */ ?>
+					on <?php the_time('l, F jS, Y') ?> at <?php the_time() ?>
+					and is filed under <?php the_category(', ') ?>.
+					You can follow any responses to this entry through the <?php comments_rss_link('RSS 2.0'); ?> feed.
 
-						<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-							// Both Comments and Pings are open ?>
-							You can <a href="#respond">leave a response</a>, or <a href="<?php trackback_url(); ?>" rel="trackback">trackback</a> from your own site.
+					<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
+						// Both Comments and Pings are open ?>
+						You can <a href="#respond">leave a response</a>, or <a href="<?php trackback_url(); ?>" rel="trackback">trackback</a> from your own site.
 
-						<?php } elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
-							// Only Pings are Open ?>
-							Responses are currently closed, but you can <a href="<?php trackback_url(); ?> " rel="trackback">trackback</a> from your own site.
+					<?php } elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
+						// Only Pings are Open ?>
+						Responses are currently closed, but you can <a href="<?php trackback_url(); ?> " rel="trackback">trackback</a> from your own site.
 
-						<?php } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-							// Comments are open, Pings are not ?>
-							You can skip to the end and leave a response. Pinging is currently not allowed.
+					<?php } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
+						// Comments are open, Pings are not ?>
+						You can skip to the end and leave a response. Pinging is currently not allowed.
 
-						<?php } elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
-							// Neither Comments, nor Pings are open ?>
-							Both comments and pings are currently closed.
+					<?php } elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
+						// Neither Comments, nor Pings are open ?>
+						Both comments and pings are currently closed.
 
-						<?php } edit_post_link('Edit this entry.','',''); ?>
+					<?php } edit_post_link('Edit this entry.','',''); ?>
 
-					</small>
-				</p>
+				</small>
+			</p>
 
-			</div>
 		</div>
 		</div>
 
