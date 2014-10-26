f44a56fbd249acb4a2f33d8a5b861eac9ae62d4e
mikekasprzak <mikekasprzak@gmail.com>
2014-10-26 19:12:31 +0000

    New neutral color scheme

--- compo/themes/ludum/index.php
+++ compo/themes/ludum/index.php
@@ -41,12 +41,15 @@
 				<?php } else { ?>
 					<div class="post" id="post-<?php the_ID(); ?>" >
 				<?php } ?>
-					<div style="float: right;"><?php echo get_avatar(get_the_author_id(),$size='56',$default='' ); ?></div>
-					<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-	                	<div>Posted by <?php the_author_posts_link(); ?><?php $aff = get_the_author_meta('affiliation', get_the_author_ID()); if (($aff != null) && ($aff != '')) { echo ' of ' . $aff; } ?><?php $twitter = get_the_author_meta('twitter', get_the_author_ID()); if (($twitter != null) && ($twitter != '')) { echo ' (twitter: <a target="_blank" href="http://twitter.com/' . $twitter . '">@' . $twitter . '</a>)'; } ?></div>
-	
 					<?php $minimize = get_post_meta($post->ID,'minimize',false); ?>
+				
+					<div class="header">
+						<div style="float: right;"><?php echo get_avatar(get_the_author_id(),$size='56',$default='' ); ?></div>
+						<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+		                <div>Posted by <?php the_author_posts_link(); ?><?php $aff = get_the_author_meta('affiliation', get_the_author_ID()); if (($aff != null) && ($aff != '')) { echo ' of ' . $aff; } ?><?php $twitter = get_the_author_meta('twitter', get_the_author_ID()); if (($twitter != null) && ($twitter != '')) { echo ' (twitter: <a target="_blank" href="http://twitter.com/' . $twitter . '">@' . $twitter . '</a>)'; } ?></div>
 					<?php if ( count($minimize) == 0 ) { ?>
+					</div>
+					<div class="body">
 						<small><?php the_time('F jS, Y g:i a') ?> <!-- by <?php the_author() ?> --></small>
 	
 						<?php echo my_get_buttons(); ?>
@@ -59,6 +62,7 @@
 					<?php } else { ?>
 						<small><?php the_time('F jS, Y g:i a') ?> | <?php if(function_exists(getILikeCount)) echo getILikeCount('get') . " love"; ?> | <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></small>				
 					<?php } ?>
+					</div>
 				</div>
 	
 			<?php endwhile; ?>
