2916747344fd979ae484b82acfcdd36c5062fd30
mikekasprzak <mikekasprzak@gmail.com>
2014-10-27 03:22:46 +0000

    New neutral color scheme

--- compo/themes/ludum/page.php
+++ compo/themes/ludum/page.php
@@ -4,7 +4,7 @@
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="ld-post post" id="post-<?php the_ID(); ?>">
-		<h2><?php the_title(); ?></h2>
+			<div><h2><?php the_title(); ?></h2></div>
 			<div class="entry">
 				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
 
