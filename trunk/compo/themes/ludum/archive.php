f34a3c51746c9003b4c9418ff879023863d9b2b5
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-10-27 07:00:59 +0000

    New neutral color scheme

--- trunk/compo/themes/ludum/archive.php
+++ trunk/compo/themes/ludum/archive.php
@@ -124,23 +124,23 @@ if (is_author() && is_category()) {
 		<?php } else { ?>
 			<div class="ld-post post">
 		<?php } ?>
-			<div class="header">
-				<div style="float: right;"><?php echo get_avatar(get_the_author_id(),$size='56',$default='' ); ?></div>
-				<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-	            <div>Posted by <?php the_author_posts_link(); ?><?php $aff = get_the_author_meta('affiliation', get_the_author_ID()); if (($aff != null) && ($aff != '')) { echo ' of ' . $aff; } ?><?php $twitter = get_the_author_meta('twitter', get_the_author_ID()); if (($twitter != null) && ($twitter != '')) { echo ' (twitter: <a target="_blank" href="http://twitter.com/' . $twitter . '">@' . $twitter . '</a>)'; } ?></div>
-				<small><?php the_time('l, F jS, Y g:i a') ?></small>
-			</div>
-            <div class="body">              
-				<?php echo my_get_buttons(); ?>
-				<div class="entry">
-					<?php the_content() ?>
+				<div class="header">
+					<div style="float: right;"><?php echo get_avatar(get_the_author_id(),$size='56',$default='' ); ?></div>
+					<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+		            <div>Posted by <?php the_author_posts_link(); ?><?php $aff = get_the_author_meta('affiliation', get_the_author_ID()); if (($aff != null) && ($aff != '')) { echo ' of ' . $aff; } ?><?php $twitter = get_the_author_meta('twitter', get_the_author_ID()); if (($twitter != null) && ($twitter != '')) { echo ' (twitter: <a target="_blank" href="http://twitter.com/' . $twitter . '">@' . $twitter . '</a>)'; } ?></div>
+					<small><?php the_time('l, F jS, Y g:i a') ?></small>
 				</div>
-				<?php echo my_get_buttons(); ?>
+	            <div class="body">              
+					<?php echo my_get_buttons(); ?>
+					<div class="entry">
+						<?php the_content() ?>
+					</div>
+					<?php echo my_get_buttons(); ?>
+				</div>
+				<div class="footer">
+					<div class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' |'); ?><?php if(function_exists(getILikeThis)) getILikeThis('get'); ?> | <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></div>
+				</div>
 			</div>
-			<div class="footer">
-				<div class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' |'); ?><?php if(function_exists(getILikeThis)) getILikeThis('get'); ?> | <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></div>			
-			</div>
-
 		<?php endwhile; ?>
 
 		<div class="navigation">
