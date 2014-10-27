84d08aac963352b0bd9047c502e8a876219e2177
mikekasprzak <mikekasprzak@gmail.com>
2014-10-27 07:02:47 +0000

    New neutral color scheme

--- compo/themes/ludum/archive.php
+++ compo/themes/ludum/archive.php
@@ -18,7 +18,7 @@
                 $auth = get_userdata($uid);
                 echo $auth->display_name;
 		?><?php $aff = get_the_author_meta('affiliation', $uid); if (($aff != null) && ($aff != '')) { echo ' of ' . $aff; } ?><?php $twitter = get_the_author_meta('twitter', $uid); if (($twitter != null) && ($twitter != '')) { echo ' (twitter: <a target="_blank" href="http://twitter.com/' . $twitter . '">@' . $twitter . '</a>)'; } ?></h2>
-		<div class="post" id="description">
+		<div class="ld-post post" id="description">
 			<?php echo wpautop($auth->description); ?>
 		</div>
 		
@@ -30,7 +30,7 @@
                 echo $auth->display_name;
 		?>'s Trophies</h2>
 		<?php } ?>
-<div class="post" id="trophies">
+<div class="ld-post post" id="trophies">
 <?php
 if (is_author() && is_category()) {
 ?>
