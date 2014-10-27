14e88e7f047dd9ad6b790b9fa6d2ef2e416fb043
mikekasprzak <mikekasprzak@gmail.com>
2014-10-27 01:51:59 +0000

    New neutral color scheme

--- compo/themes/ludum/footer.php
+++ compo/themes/ludum/footer.php
@@ -1,13 +1,9 @@
-	<hr />
-	<div id="footer">
-		<p style="color: #ffffff; text-align:center;">
-			All posts, images, and comments are owned by their creators.<br />
-			<?php echo get_num_queries(); ?> queries executed in <?php timer_stop(1); ?> seconds.
-		</p>
+		<div id="footer">
+			<div class="text">All posts, images, and comments are owned by their creators.</div>
+			<div class="query"><?php echo get_num_queries(); ?> queries executed in <?php timer_stop(1); ?> seconds.</div>
+		</div>
 	</div>
-</div>
-
-		<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 </body>
 </html>
 <?php do_action("compo2_cache_end"); ?>
