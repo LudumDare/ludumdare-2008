fae3a3d77831028cde95a0abaa7647828d4684e6
mikekasprzak <mikekasprzak@gmail.com>
2014-11-05 23:48:57 +0000

    page borders

--- compo/themes/ludum/page.php
+++ compo/themes/ludum/page.php
@@ -2,7 +2,8 @@
 <div id="body">
 	<div id="content" class="fullcolumn botpad">
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php 	if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php 		if ( get_post_meta(get_the_ID(),'border',true) !== "false" ) { ?>
 		<div class="ld-post post" id="post-<?php the_ID(); ?>">
 			<div class="head"><h2><?php the_title(); ?></h2></div>
 			<div class="entry">
@@ -10,6 +11,12 @@
 				<br />
 			</div>
 		</div>
+<?php		} ?>
+<?php		else { ?>
+		<div class="entry">
+			<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+		</div>
+<?php		} ?>
 		<?php endwhile; endif; ?>
 	<?php edit_post_link('Edit this entry.', '<div>', '</div>'); ?>
 	</div>
