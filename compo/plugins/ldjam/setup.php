f88888c7967b98d939a777d3b77a51aecfc489d8
mikekasprzak <mikekasprzak@gmail.com>
2014-11-08 07:32:21 +0000

    meh

--- compo/plugins/ldjam/setup.php
+++ compo/plugins/ldjam/setup.php
@@ -1,10 +1,29 @@
 <?php
 defined('ABSPATH') or die("No.");
 
+require_once "config.php";
+
+$ld_data = NULL;
+
+
 function ld_setup() {
+	//if ( 
+}
+
+
+function ld_has_data_table() {
+//	return ld_db_
+}
+function ld_new_data_table() {	
+}
+function ld_get_data_table() {
 	
 }
 
+function ld_has_content_table() {
+}
+function ld_new_content_table() {	
+}
 
 //		$ld_table_prefix = "ld_";
 //		$content_table_name = $ld_table_prefix . "content";
