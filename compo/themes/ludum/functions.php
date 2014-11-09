f44b90f42d964f4b81e889f6ab403d130f7124a1
mikekasprzak <mikekasprzak@gmail.com>
2014-11-09 17:01:08 +0000

    dumb function

--- compo/themes/ludum/functions.php
+++ compo/themes/ludum/functions.php
@@ -430,7 +430,7 @@ function ludumtheme_comment($comment, $args, $depth) {
       <?php comment_text() ?>
 
       <div class="avatar">
-      	<?php if(function_exists(ckrating_display_karma)) { ckrating_display_karma(); }  ?>
+      	<?php if(function_exists('ckrating_display_karma')) { ckrating_display_karma(); }  ?>
       </div>
       <div class="reply">
          <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
