80dddbfeb9e62dad52084b6123e1fb4baa15398c
mikekasprzak <mikekasprzak@gmail.com>
2014-11-10 02:00:03 +0000

    no mroe setup

--- compo/plugins/ldjam/lib.php
+++ compo/plugins/ldjam/lib.php
@@ -16,8 +16,8 @@ function to_bool( $value ) {
 // - ----------------------------------------------------------------------------------------- - //
 function to_slug( $str, $delimiter='-' ) {
 	$clean = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
-	$clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
-	$clean = strtolower(trim($clean, '-'));
+//	$clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+//	$clean = strtolower(trim($clean, '-'));
 //	$clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
 	
 	return $clean;
