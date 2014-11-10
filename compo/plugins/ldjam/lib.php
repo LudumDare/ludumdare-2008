aaded79690a4d142f05e4a82a8536c7f36e0cef6
mikekasprzak <mikekasprzak@gmail.com>
2014-11-10 04:34:22 +0000

    zpiffy

--- compo/plugins/ldjam/lib.php
+++ compo/plugins/ldjam/lib.php
@@ -25,4 +25,49 @@ function to_slug( $str, $delimiter='-' /*, $maxlength=260*/ ) {
 	return $clean;
 }
 // - ----------------------------------------------------------------------------------------- - //
+
+// - ----------------------------------------------------------------------------------------- - //
+// http://programanddesign.com/php/base62-encode/
+// MK: Removed 'iuIU' to make it harder to generate F-bombs, S-bombs, and C-bombs //
+// - ----------------------------------------------------------------------------------------- - //
+/**
+ * Converts a base 10 number to any other base.
+ * 
+ * @param int $val   Decimal number
+ * @param int $base  Base to convert to. If null, will use strlen($chars) as base.
+ * @param string $chars Characters used in base, arranged lowest to highest. Must be at least $base characters long.
+ * 
+ * @return string    Number converted to specified base
+ */
+function base_encode($val, $base=58, $chars='0123456789abcdefghjklmnopqrstvwxyzABCDEFGHJKLMNOPQRSTVWXYZ') {
+    if(!isset($base)) $base = strlen($chars);
+    $str = '';
+    do {
+        $m = bcmod($val, $base);
+        $str = $chars[$m] . $str;
+        $val = bcdiv(bcsub($val, $m), $base);
+    } while(bccomp($val,0)>0);
+    return $str;
+}
+// - ----------------------------------------------------------------------------------------- - //
+/**
+ * Convert a number from any base to base 10
+ * 
+ * @param string $str   Number
+ * @param int $base  Base of number. If null, will use strlen($chars) as base.
+ * @param string $chars Characters use in base, arranged lowest to highest. Must be at least $base characters long.
+ * 
+ * @return int    Number converted to base 10
+ */
+function base_decode($str, $base=58, $chars='0123456789abcdefghjklmnopqrstvwxyzABCDEFGHJKLMNOPQRSTVWXYZ') {
+    if(!isset($base)) $base = strlen($chars);
+    $len = strlen($str);
+    $val = 0;
+    $arr = array_flip(str_split($chars));
+    for($i = 0; $i < $len; ++$i) {
+        $val = bcadd($val, bcmul($arr[$str[$i]], bcpow($base, $len-$i-1)));
+    }
+    return $val;
+}
+// - ----------------------------------------------------------------------------------------- - //
 ?>
