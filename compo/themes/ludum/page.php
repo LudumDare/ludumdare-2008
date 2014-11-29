5df2f9f628b8416751d9df9205e4618c0ec88d82
mikekasprzak <mikekasprzak@gmail.com>
2014-11-29 23:26:56 +0000

    thin

--- compo/themes/ludum/page.php
+++ compo/themes/ludum/page.php
@@ -9,14 +9,14 @@
 <?php 	if (have_posts()) : while (have_posts()) : the_post(); ?>
 <?php 		if ( get_post_meta(get_the_ID(),'border',true) !== "false" ) { ?>
 		<div class="ld-post post" id="post-<?php the_ID(); ?>">
-			<div class="head"><h2><?php the_title(); ?></h2></div>
-			<div class="entry">
+			<div class="head header"><h2><?php the_title(); ?></h2></div>
+			<div class="entry body">
 				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
 				<br />
 			</div>
 		</div>
 <?php		} else { ?>
-		<div class="entry">
+		<div class="entry body">
 			<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
 		</div>
 <?php		} ?>
