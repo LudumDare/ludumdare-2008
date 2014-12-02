6203ef39c0258670ca07680d988c9cf665d06871
mikekasprzak <mikekasprzak@gmail.com>
2014-12-02 04:14:05 +0000

    can now view themes w/o being logged in

--- compo/plugins/compo/vote.php
+++ compo/plugins/compo/vote.php
@@ -79,7 +79,7 @@ function _compo_vote_results($pid) {
 		});
 		
 		if ( function_exists('apcu_store') ) {
-			apcu_store( 'c1_vote_result_' . $pid, $data, 180);	// Store for 3 minutes //
+			apcu_store( 'c1_vote_result_' . $pid, $data);
 		}		
 	}
 	
