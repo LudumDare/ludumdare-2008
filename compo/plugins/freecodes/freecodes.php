3189363cdb3399d9cad2acf4d6595ede0d013812
mikekasprzak <mikekasprzak@gmail.com>
2014-12-05 03:02:10 +0000

    Added FreeCodes plugin

--- compo/plugins/freecodes/freecodes.php
+++ compo/plugins/freecodes/freecodes.php
@@ -27,7 +27,7 @@ function show_freecodes(){
 	
 	if ( is_user_logged_in() ) {
 		$slug = get_post( $post )->post_name;
-		$user = wp_get_current_user();		
+		$user = get_current_user_id();		
 		
 		$code = get_freecodes($user,$slug);
 		
@@ -37,12 +37,19 @@ function show_freecodes(){
 		}
 		else {
 			echo 'I need a code';
+			echo '
+				<form action="" method="POST" name="get_code" class="freecodes-get">
+					<input id="uid" type="hidden" name="pid" value="{$user}" />
+					<input id="GET_CODE" type="hidden" name="GET_CODE" value="GET_CODE" />
+					<input id="submit" type="submit" name="submit" value="Get a Code" class="freecodes-button" />
+				</form>
+			';
 		}		
 //		//only print fi admin
 //		if (current_user_can('edit_others_posts')){
 //			echo '
 //			<form action="" method="POST" name="get_code" class="freecodes-get">
-//				<input id="pid" type="hidden" name="pid" value="'.$post->ID.'" />
+//				<input id="uid" type="hidden" name="pid" value="{$user}" />
 //				<input id="GET_CODE" type="hidden" name="GET_CODE" value="GET_CODE" />
 //				<input id="submit" type="submit" name="submit" value="Get a Code" class="freecodes-button" />
 //			</form>';
@@ -59,7 +66,7 @@ add_shortcode( 'freecodes', 'show_freecodes' );
 
 function init_freecodes() {	
 	if (isset($_POST['GET_CODE']) && $_POST['GET_CODE'] == 'GET_CODE'){
-		if (isset($_POST['pid']) && !empty($_POST['pid'])){
+		if (isset($_POST['uid']) && !empty($_POST['uid'])){
 			//change_post_status((int)$_POST['pid'],'publish');
 		}
 	}
