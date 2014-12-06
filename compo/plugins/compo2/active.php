bedf5e1824a28f397b3146b363759c06df6383d8
mikekasprzak <mikekasprzak@gmail.com>
2014-12-06 18:17:12 +0000

    added restrictions to submission

--- compo/plugins/compo2/active.php
+++ compo/plugins/compo2/active.php
@@ -122,12 +122,12 @@ function _compo2_active_form($params,$uid="",$is_admin=0) {
             echo "<div>&nbsp;</div>";
             
             $idx = 0;
-            $requirement = null;
-            if ( $params[$div."_req"] !== "" ) {
+            $requirement = [];
+            if ( $params[$div."_req"] ) {
 	            $requirement = explode(";",$params[$div."_req"]);
 	        }
             foreach ($requirement as $req) {
-	            echo "<input type='checkbox' name='{$div}_REQ_{$idx}' value='{$div}_REQ_{$idx}'>{$req}</input>";
+	            echo "<input type='checkbox' name='{$div}_REQ_{$idx}' value='{$div}_REQ_{$idx}'>{$req}</input><br />";
 	            $idx++;           	
             }
             // MK: END NIGHT
