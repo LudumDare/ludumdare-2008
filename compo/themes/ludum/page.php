40a58321c6cee4013b712984e732f01fe35afdc4
mikekasprzak <mikekasprzak@gmail.com>
2014-12-07 17:57:15 +0000

    submission form fun

--- compo/themes/ludum/page.php
+++ compo/themes/ludum/page.php
@@ -9,7 +9,7 @@
 <?php 	if (have_posts()) : while (have_posts()) : the_post(); ?>
 <?php 	if ( get_post_meta(get_the_ID(),'border',true) !== "false" ) { ?>
 		<div class="ld-post post" id="post-<?php the_ID(); ?>">
-<?php 	if ( get_post_meta(get_the_ID(),'notitle',true) !== "true" ) { ?>
+<?php 	if ( get_post_meta(get_the_ID(),'notitle',true) != "" ) { ?>
 			<div class="head header"><h2><?php the_title(); ?></h2></div>
 <?php	} ?>
 			<div class="entry body">
