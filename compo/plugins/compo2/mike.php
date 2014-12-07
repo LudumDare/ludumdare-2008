19bd53cc53c1d1910871dba9aa0772a98344d6ce
mikekasprzak <mikekasprzak@gmail.com>
2014-12-07 18:56:36 +0000

    submission form fun

--- compo/plugins/compo2/mike.php
+++ compo/plugins/compo2/mike.php
@@ -90,13 +90,38 @@ function c2_navigation($slug,$name,$name_url) {
 	}
 	else {
 		if ( !is_paged() ) {
+			$user_id = get_current_user_id();
+			$event_id = 0;
+			$underscore_slug = str_replace( '-', '_', $slug );
+			if ( function_exists('apcu_fetch') && !isset($_GET["cache"]) ) {
+				$event_id = apcu_fetch('c2_slug_cache_'.$underscore_slug);
+			}
+			if ( !$event_id ) {
+				$page = get_posts(array('name'=> $slug,'post_type'=>'page'))[0];
+				
+				//print_r($page);
+				
+				$event_id = $page->ID;
+				
+				if ( function_exists('apcu_store') ) {
+					apcu_store('c2_slug_cache_'.$underscore_slug, $event_id);
+				}
+			}
+			
+			$game = c2_get_game($event_id,$user_id);
+
 ?>
 		<div class="event">
 			<div class="info">
 				<div class="navigation">
-<?php			if ( is_user_logged_in() ) { ?>
-					<a href="/compo/<?php echo $slug?>/?action=edit"><strong>Submit</strong></a> | 
-<?php			} ?>
+<?php				if ( is_user_logged_in() ) {
+						if ( $game ) {?>
+							<a href="/compo/<?php echo $slug?>/?action=edit"><strong>Edit</strong></a> | 
+<?php					}
+						else {?>
+							<a href="/compo/<?php echo $slug?>/?action=edit"><strong>Submit</strong></a> | 
+<?php					}
+					} ?>
 					<a href="/compo/<?php echo $slug?>/?action=preview">View All</a>
 				</div>
 				<div class="name">On Now: <strong><a href="<?php echo $name_url; ?>"><?php echo $name; ?></a></strong></div>
