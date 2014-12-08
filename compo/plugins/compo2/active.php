6156283d0aea65828ef9891b0d340a8b496d0fec
mikekasprzak <mikekasprzak@gmail.com>
2014-12-08 21:58:51 +0000

    Improved love

--- compo/plugins/compo2/active.php
+++ compo/plugins/compo2/active.php
@@ -363,7 +363,7 @@ function _compo2_active_form($params,$uid="",$is_admin=0) {
 	echo "<td>Max: 800";
 	echo "<td>";
 	echo "</table>";
-	echo "<input type='checkbox' class='' name='SETTING[EMBED][fullscreen]' value='1' ".($settings['EMBED']['fullscreen']?"checked":"").">Enable Full-Screen Button.<br/>Games running Full-Screen use a variety of 16:9, 16:10, and 4:3 resolutions.</input><br />";
+	echo "<input type='checkbox' class='' name='SETTING[EMBED][fullscreen]' value='1' ".($settings['EMBED']['fullscreen']?"checked":"").">Enable Fullscreen Button.<br/>People have a variety of screen sizes, so games running Fullscreen should be able to support a variety of 16:9, 16:10, and 4:3 resolutions.</input><br />";
 		
 
 /*    echo "<br />";*/
