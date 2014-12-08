6f39d39ade3ec52a8aa25404924ac5693b21af60
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-12-08 05:25:20 +0000

    all page info on my game

--- trunk/compo/plugins/compo2/mike.php
+++ trunk/compo/plugins/compo2/mike.php
@@ -37,105 +37,64 @@ function c2_set_game($event_id,$user_id,$game) {
 
 // This Function is called in the style sheet to display Navigation //
 function c2_navigation($slug,$name,$name_url) {
-	if ( current_user_can('edit_others_posts') ) {
-		//if ( !is_paged() ) 
-		{
-			$user_id = get_current_user_id();
-			$event_id = 0;
-			$underscore_slug = str_replace( '-', '_', $slug );
-			if ( function_exists('apcu_fetch') && !isset($_GET["cache"]) ) {
-				$event_id = apcu_fetch('c2_slug_cache_'.$underscore_slug);
-			}
-			if ( !$event_id ) {
-				$page = get_posts(array('name'=> $slug,'post_type'=>'page'))[0];
-				
-				//print_r($page);
-				
-				$event_id = $page->ID;
-				
-				if ( function_exists('apcu_store') ) {
-					apcu_store('c2_slug_cache_'.$underscore_slug, $event_id);
-				}
-			}
-			
-			$game = c2_get_game($event_id,$user_id);
-			
-			// MK: END NIGHT
-			/*
-			var_dump($game);
-			echo "<br/><br/>";
-			c2_set_game($event_id,$user_id,$game);
-			*/
-			
-			//echo $event_id . " vs " . $user_id;
-			
-?>
-		<div class="event">
-			<div class="info">
-				<div class="navigation">
-<?php				if ( is_user_logged_in() ) {
-						if ( $game ) {?>
-							<a href="/compo/<?php echo $slug?>/"><strong>Play and Rate Games</strong></a> | 
-							<a href="/compo/<?php echo $slug?>/?action=edit">Edit</a> | 
-							<a href="/compo/<?php echo $slug?>/?action=me">My Game</a> | 
-<?php					}
-						else {?>
-							<a style="font-size:15px" href="/compo/<?php echo $slug?>/?action=edit"><strong>SUBMIT NOW</strong></a> | 
-<?php					}
-					} ?>
-					<a href="/compo/<?php echo $slug?>/?action=preview">View All</a>
-				</div>
-				<div class="name">On Now: <strong><a href="<?php echo $name_url; ?>"><?php echo $name; ?></a></strong></div>
-			</div>
-		</div>
-<?php
+	//if ( !is_paged() ) { // First Page Only // 
+	{
+		$user_id = get_current_user_id();
+		$event_id = 0;
+		$underscore_slug = str_replace( '-', '_', $slug );
+		if ( function_exists('apcu_fetch') && !isset($_GET["cache"]) ) {
+			$event_id = apcu_fetch('c2_slug_cache_'.$underscore_slug);
 		}
-	}
-	else {
-		//if ( !is_paged() ) 
-		{
-			$user_id = get_current_user_id();
-			$event_id = 0;
-			$underscore_slug = str_replace( '-', '_', $slug );
-			if ( function_exists('apcu_fetch') && !isset($_GET["cache"]) ) {
-				$event_id = apcu_fetch('c2_slug_cache_'.$underscore_slug);
-			}
-			if ( !$event_id ) {
-				$page = get_posts(array('name'=> $slug,'post_type'=>'page'))[0];
-				
-				//print_r($page);
-				
-				$event_id = $page->ID;
-				
-				if ( function_exists('apcu_store') ) {
-					apcu_store('c2_slug_cache_'.$underscore_slug, $event_id);
-				}
-			}
+		if ( !$event_id ) {
+			$page = get_posts(array('name'=> $slug,'post_type'=>'page'))[0];
 			
-			$game = c2_get_game($event_id,$user_id);
+			//print_r($page);
+			
+			$event_id = $page->ID;
+			
+			if ( function_exists('apcu_store') ) {
+				apcu_store('c2_slug_cache_'.$underscore_slug, $event_id);
+			}
+		}
+		
+		$game = c2_get_game($event_id,$user_id);
+
+//		if ( current_user_can('edit_others_posts') ) {
+//			// MK: END NIGHT
+//			/*
+//			var_dump($game);
+//			echo "<br/><br/>";
+//			c2_set_game($event_id,$user_id,$game);
+//			*/
+//			
+//			//echo $event_id . " vs " . $user_id;
+//		}
 
 ?>
-		<div class="event">
-			<div class="info">
-				<div class="navigation" style="">
-<?php				if ( is_user_logged_in() ) {
-						if ( $game ) {?>
-							<a href="/compo/<?php echo $slug?>/"><strong>Play and Rate Games</strong></a> | 
-							<a href="/compo/<?php echo $slug?>/?action=edit">Edit</a> | 
-							<a href="/compo/<?php echo $slug?>/?action=me">My Game</a> | 
+	<div class="event">
+		<div class="info">
+			<div class="navigation" style="">
+<?php			if ( is_user_logged_in() ) {
+					if ( $game ) {?>
+						<a href="/compo/<?php echo $slug?>/"><strong>Play and Rate Games</strong></a> | 
+						<a href="/compo/<?php echo $slug?>/?action=edit">Edit</a> | 
 <?php					}
-						else {?>
-							<a style="font-size:15px" href="/compo/<?php echo $slug?>/?action=edit"><strong>SUBMIT NOW</strong></a> | 
-<?php					}
-					} ?>
-					<a href="/compo/<?php echo $slug?>/?action=preview">View All</a>
-				</div>
-				<div class="name">On Now: <strong><a href="<?php echo $name_url; ?>"><?php echo $name; ?></a></strong></div>
+				else {?>
+						<a style="font-size:15px" href="/compo/<?php echo $slug?>/?action=edit"><strong>SUBMIT NOW</strong></a> | 
+<?php				}
+				} ?>
+				<a href="/compo/<?php echo $slug?>/?action=preview">View All</a>
 			</div>
+			<div class="name">On Now: <strong><a href="<?php echo $name_url; ?>"><?php echo $name; ?></a></strong></div>
+<?php		if ( is_user_logged_in() ) {
+				if ( $game ) {?>
+					<div class="name">My Game: <strong><a href="/compo/<?php echo $slug?>/?action=preview&uid=<?php echo $game['uid'];?>"><?php echo $game['title']; ?></a></strong></div>
+<?php			}
+			} ?>
 		</div>
+	</div>
 <?php
-		}
-	}	
+	}
 }
 
 ?>
