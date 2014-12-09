e1add9294373959b7706689f4a590acabafa4558
mikekasprzak <mikekasprzak@gmail.com>
2014-12-09 00:14:59 +0000

    flipping opt-outs

--- compo/plugins/compo2/mike.php
+++ compo/plugins/compo2/mike.php
@@ -59,16 +59,16 @@ function c2_navigation($slug,$name,$name_url) {
 		
 		$game = c2_get_game($event_id,$user_id);
 
-//		if ( current_user_can('edit_others_posts') ) {
-//			// MK: END NIGHT
-//			/*
-//			var_dump($game);
-//			echo "<br/><br/>";
-//			c2_set_game($event_id,$user_id,$game);
-//			*/
-//			
-//			//echo $event_id . " vs " . $user_id;
-//		}
+		if ( current_user_can('edit_others_posts') ) {
+			// MK: END NIGHT
+			var_dump($game);
+			/*
+			echo "<br/><br/>";
+			c2_set_game($event_id,$user_id,$game);
+			*/
+			
+			//echo $event_id . " vs " . $user_id;
+		}
 
 ?>
 	<div class="event">
