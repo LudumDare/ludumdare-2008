81e56be7f08ea9c184a94e01e78165ff74ca81c9
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-12-10 07:43:08 +0000

    almost

--- trunk/compo/plugins/compo2/preview.php
+++ trunk/compo/plugins/compo2/preview.php
@@ -323,7 +323,7 @@ function _compo2_preview_show($params,$uid,$comments=true) {
 	if ( current_user_can('edit_others_posts') ) {
 		//echo "Hey team. Just ignore this for now. Only you can see it. Thanks!<br /><br />";
 		//var_dump( $divs );
-		echo "TS:". date('Y-m-d H:i:s',$cs['ts']);
+		echo "TS:". strtotime($cs['ts']);//date('Y-m-d H:i:s',$cs['ts']);
 	}
 		
 	echo "<table>";
