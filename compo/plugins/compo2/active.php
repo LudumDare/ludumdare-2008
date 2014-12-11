082dbba3650f674d20f33343c61b2807b9015dee
mikekasprzak <mikekasprzak@gmail.com>
2014-12-11 01:53:46 +0000

    back to the box

--- compo/plugins/compo2/active.php
+++ compo/plugins/compo2/active.php
@@ -334,7 +334,7 @@ function _compo2_active_form($params,$uid="",$is_admin=0) {
 
 	// Thumbnail //
 	echo "<h2>Customize Thumbnail</h2>";    
-	echo "Game thumbnails are <strong>180x140</strong> pixel. Image will be scaled and cropped. <strong>TIP</strong>: If you use an exact 180x140 <strong>GIF</strong>, it will animate on hover.<br />";
+	echo "Game thumbnails are <strong>180x140</strong> pixels (9:7). Images will be scaled and cropped. <strong>TIP</strong>: If you use an exact 180x140 <strong>GIF</strong>, it will animate on hover.<br />";
 	echo "If you don't set a thumbnail, Screenshot #1 will be used instead (scaled and cropped).<br /><br />";
 
 	echo "<table>";
