45d5e786185abe3b283c2430737b8046fb7ed63d
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2014-12-11 06:27:10 +0000

    back to the box

--- trunk/compo/plugins/compo2/preview.php
+++ trunk/compo/plugins/compo2/preview.php
@@ -376,8 +376,8 @@ function _compo2_preview_show($params,$uid,$comments=true) {
 			margin:0;
 		}
 		
-		.links ul li:before {
-			content:'';
+		.links ul {
+			list-style:none;
 		}
 	</style>
 	";
