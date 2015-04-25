0abe4ebd28b013fe98b300e3b152f6468fcba28e
mikekasprzak <mikekasprzak@gmail.com>
2015-04-25 18:18:08 +0000

    centered the thumbnail

--- compo/plugins/compo2/preview.php
+++ compo/plugins/compo2/preview.php
@@ -277,7 +277,7 @@ function _compo2_preview_show($params,$uid,$comments=true) {
 		$imagefile = array_values($shots)[0];
 		$shot_url = c2_thumb($imagefile,900,500,false,true);
 		
-		$play_code = "<div id='embed' onclick='c2_play_game();' style='width:{$width}px;height:{$height}px'><img src='{$shot_url}'><div class='embed-overlay'></div><img class='embed-icon' src='/compo/wp-content/plugins/compo2/art/play.png' /></div>";
+		$play_code = "<div id='embed' onclick='c2_play_game();' style='width:{$width}px;height:{$height}px'><img class='embed-icon' src='{$shot_url}'><div class='embed-overlay'></div><img class='embed-icon' src='/compo/wp-content/plugins/compo2/art/play.png' /></div>";
 		
 ?>
 		<script>
