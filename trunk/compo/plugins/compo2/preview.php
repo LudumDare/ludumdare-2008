7df3b0f412a0bc9190c3a3ff27d015a4e8d1c2da
mikekasprzak@gmail.com <mikekasprzak@gmail.com@a6a53419-064e-0410-8e23-07da878cd106>
2015-04-25 06:57:13 +0000

    Working on the Game Controls

--- trunk/compo/plugins/compo2/preview.php
+++ trunk/compo/plugins/compo2/preview.php
@@ -356,8 +356,8 @@ function _compo2_preview_show($params,$uid,$comments=true) {
 		echo "<div id='embed-frame'>";
 			echo "<div id='embed' onclick='c2_play_game();'>
 				<img src='{$shot_url}' width='{$width}' height='{$height}'>
-				<div class='sn-overlay'></div>
-				<img class='sn-icon' src='/compo/wp-content/plugins/compo2/art/play.png' />
+				<div class='overlay'></div>
+				<img class='icon' src='/compo/wp-content/plugins/compo2/art/play.png' />
 			</div>";
 		echo "</div>";	
 		echo "<div class='embed-controls'>";		
