50f057c97ef4a52b7386d12c2026f56d5093af33
mikekasprzak <mikekasprzak@gmail.com>
2015-05-06 18:15:38 +0000

    dumping data on rating

--- compo/plugins/compo2/rate.php
+++ compo/plugins/compo2/rate.php
@@ -477,6 +477,24 @@ function _compo2_rate_io_calc($params,$uid) {
 
     $cc = array_pop(compo2_query("select count(*) cnt from c2_rate where cid = ? and from_uid = ?",array($cid,$uid)));
     $out = $cc["cnt"];
+
+    echo "<!-- RAWG -->\n";
+    $allcc = compo2_query("select count(*) cnt from c2_rate where cid = ? and from_uid = ?",array($cid,$uid));
+    $all = array();
+    foreach ($allcc as $ve) {
+    	$data = unserialize($ve['data']);
+    	$sum = 0;
+    	foreach ($data as $part) {
+    		$sum += intval($part);
+    	}
+    	if ( $sum > 0 ) {
+    		$all[] = $data;
+    	}
+    }
+//    echo "<!--\n";
+//    print_r( $all );
+//    echo "-->\n";
+    echo "<!-- New Total: " . count($all) . " vs Total: " . $out . " -->\n";
     
     compo2_update("c2_entry",array(
         "id"=>$ce["id"],
