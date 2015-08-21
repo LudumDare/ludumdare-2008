05862b933c8b3c293a847f7f0dc55abffcf24755
Mike Kasprzak <mike@sykhronics.com>
2015-08-21 11:02:04 -0400

    hopefully cleaned up the registration e-mail

--- compo/plugins/sabre/sabre.php
+++ compo/plugins/sabre/sabre.php
@@ -52,6 +52,57 @@ if (class_exists("Sabre")) {
 /***********************************************************************/
 /* Redefine user notification function                                 */
 /***********************************************************************/
+
+
+
+if ( !function_exists('wp_new_user_notification') ) :
+/**
+ * Email login credentials to a newly-registered user.
+ *
+ * A new user registration notification is also sent to admin email.
+ *
+ * @since 2.0.0
+ * @since 4.3.0 The `$plaintext_pass` parameter was changed to `$notify`.
+ *
+ * @param int    $user_id User ID.
+ * @param string $notify  Optional. Type of notification that should happen. Accepts 'admin' or an empty
+ *                        string (admin only), or 'both' (admin and user). The empty string value was kept
+ *                        for backward-compatibility purposes with the renamed parameter. Default empty.
+ */
+function wp_new_user_notification( $user_id, $notify = '' ) {
+	global $wpdb;
+	$user = get_userdata( $user_id );
+	// The blogname option is escaped with esc_html on the way into the database in sanitize_option
+	// we want to reverse this for the plain text arena of emails.
+	$blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
+	$message  = sprintf(__('New user registration on your site %s:'), $blogname) . "\r\n\r\n";
+	$message .= sprintf(__('Username: %s'), $user->user_login) . "\r\n\r\n";
+	$message .= sprintf(__('E-mail: %s'), $user->user_email) . "\r\n";
+	@wp_mail(get_option('admin_email'), sprintf(__('[%s] New User Registration'), $blogname), $message);
+	if ( 'admin' === $notify || empty( $notify ) ) {
+		return;
+	}
+	// Generate something random for a password reset key.
+	$key = wp_generate_password( 20, false );
+	/** This action is documented in wp-login.php */
+	do_action( 'retrieve_password_key', $user->user_login, $key );
+	// Now insert the key, hashed, into the DB.
+	if ( empty( $wp_hasher ) ) {
+		require_once ABSPATH . WPINC . '/class-phpass.php';
+		$wp_hasher = new PasswordHash( 8, true );
+	}
+	$hashed = time() . ':' . $wp_hasher->HashPassword( $key );
+	$wpdb->update( $wpdb->users, array( 'user_activation_key' => $hashed ), array( 'user_login' => $user->user_login ) );
+	$message = sprintf(__('Username: %s'), $user->user_login) . "\r\n\r\n";
+	$message .= __('To set your password, visit the following address:') . "\r\n\r\n";
+	$message .= network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user->user_login), 'login') . "\r\n\r\n";
+//	$message .= wp_login_url() . "\r\n";
+	$message .= __('Make sure you click the RESET PASSWORD button to save your password.') . "\r\n\r\n";
+	wp_mail($user->user_email, sprintf(__('[%s] Your username and password info'), $blogname), $message);
+}
+endif;
+
+
 /*
 if ( !function_exists('wp_new_user_notification') && isset($mySabre) ) :
 function wp_new_user_notification($user_id, $plaintext_pass = '') {
