01c9ff8bddf4be2108a51c33aed1d3c1edbf34e0
Mike Kasprzak <mike@sykhronics.com>
2015-11-12 04:26:13 -0500

    trying validation again...

--- compo/plugins/ldjam/validate-user.php
+++ compo/plugins/ldjam/validate-user.php
@@ -5,5 +5,9 @@
 setcookie( "lusha", "1234", 2*24*60*60, "/", "theme.ludumdare.com" );
 
 // Redirect //
-header("Location: http://theme.ludumdare.com");
+//header("Location: http://theme.ludumdare.com");
+
+echo '<!doctype html>';
+echo '<html><head><meta http-equiv="Location" content="http://example.com/"></head>';
+echo '<body><a href="http://theme.ludumdare.com">http://theme.ludumdare.com</a></body></html>';
 die();
