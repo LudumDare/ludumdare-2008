21e0e2dd723a41c51a975b5087bad9ffca9d3848
Mike Kasprzak <mike@sykhronics.com>
2015-11-12 04:43:12 -0500

    back to location redirecting

--- compo/plugins/ldjam/validate-user.php
+++ compo/plugins/ldjam/validate-user.php
@@ -5,9 +5,9 @@
 setcookie( "lusha", "1234", time()+2*24*60*60, "/", ".ludumdare.com" );
 
 // Redirect //
-//header("Location: http://theme.ludumdare.com");
+header("Location: http://theme.ludumdare.com");
 
-echo '<!doctype html>';
-echo '<html><head><meta http-equiv="Location" content="http://example.com/"></head>';
-echo '<body><a href="http://theme.ludumdare.com">http://theme.ludumdare.com</a></body></html>';
+//echo '<!doctype html>';
+//echo '<html><head><meta http-equiv="Location" content="http://example.com/"></head>';
+//echo '<body><a href="http://theme.ludumdare.com">http://theme.ludumdare.com</a></body></html>';
 die();
