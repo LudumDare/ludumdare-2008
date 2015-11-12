846b653005251d9edf1300e5a94ad55c38565fcd
Mike Kasprzak <mike@sykhronics.com>
2015-11-12 04:31:47 -0500

    trying validation again...

--- compo/plugins/ldjam/validate-user.php
+++ compo/plugins/ldjam/validate-user.php
@@ -2,7 +2,7 @@
 // Check wordpress credentials //
 
 // Set Cookie //
-setcookie( "lusha", "1234", time()+2*24*60*60, "/", "theme.ludumdare.com" );
+setcookie( "lusha", "1234", time()+2*24*60*60, "/", ".ludumdare.com" );
 
 // Redirect //
 //header("Location: http://theme.ludumdare.com");
