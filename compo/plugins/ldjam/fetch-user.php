7037d65a0b2373e5b9ee0d4668efec0862b83041
Mike Kasprzak <mike@sykhronics.com>
2015-11-12 13:40:24 -0500

    Added handshake test

--- compo/plugins/ldjam/fetch-user.php
+++ compo/plugins/ldjam/fetch-user.php
@@ -1,6 +1,22 @@
 <?php
-$allowed_ip = [
-	"45.79.188.170"
-];
+require_once __DIR__."/config.php";
+
+// Shorthand function for checking a configuration whitelist //
+function core_OnWhitelist( $ip, $list ) {
+	if ( is_string($list) ) {
+		$list = [$list];
+	}
+	
+	foreach ( $list as $item ) {
+		if ( $item == $ip )
+			return true;
+	}
+	
+	return false;
+}
+
+//if (core_OnWhitelist($_SERVER['REMOTE_ADDR'],$IP_WHITELIST)) {
+
+echo '{"isreal":true}';
 
 ?>
