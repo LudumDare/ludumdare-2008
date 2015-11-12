09c154b4fb9b1c958fef1654c058d9cda43f40a0
Mike Kasprzak <mike@sykhronics.com>
2015-11-12 04:18:43 -0500

    Dummy user validate

--- compo/plugins/ldjam/validate-user.php
+++ compo/plugins/ldjam/validate-user.php
@@ -1,4 +1,9 @@
 <?php
-	// Check wordpress credentials and redirect //
+// Check wordpress credentials //
 
-?>
+// Set Cookie //
+setcookie( "lusha", "1234", 2*24*60*60, "/", "theme.ludumdare.com" );
+
+// Redirect //
+header("Location: http://theme.ludumdare.com");
+die();
