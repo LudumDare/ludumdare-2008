d5348900c746b53095fe29b712195885226e1806
Mike Kasprzak <mike@sykhronics.com>
2015-11-12 16:02:49 -0500

    testing the wordpress side

--- compo/plugins/ldjam/validate-user.php
+++ compo/plugins/ldjam/validate-user.php
@@ -1,8 +1,38 @@
 <?php
 // Check wordpress credentials //
 require_once __DIR__."/../../../wp-load.php";
+require_once __DIR__."config.php";
 
-var_dump( wp_get_current_user() );
+// TODO: check if not logged in 
+
+$wp_user = wp_get_current_user();
+
+$data = [
+	'id' => $wp_user['ID'],
+//	'name' => $wp_user['data']['display_name'],
+//	'slug' => $wp_user['data']['user_nicename'],
+//	'mail' => $wp_user['data']['user_email'],
+	'gravatar' => md5(strtolower(trim( $wp_user['data']['user_email'] ))),
+//	'register_date' => $wp_user['data']['user_registered'],
+//	'login' => $wp_user['data']['user_login'],
+];
+
+//$wp_user['caps']['administrator']
+//$wp_user['roles'] // Array of strings including 'administrator'
+
+
+//	$query = [
+//		'action'=>"LEGACY_FETCH",
+//		'id'=>$id
+//	];
+//	
+//	$result = http_post_fields(LEGACY_FETCH_URL,$query);
+//	
+//	if ( $result !== false ) {
+//		var_dump($result);
+//		return json_decode($result);
+//	}
+
 
 // Set Cookie //
 //setcookie( "lusha", "1234", time()+2*24*60*60, "/", ".ludumdare.com" );
