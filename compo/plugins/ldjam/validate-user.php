fd2faadd3b2a481ec24682e7f9e0b2def5571c69
Mike Kasprzak <mike@sykhronics.com>
2015-11-12 15:45:23 -0500

    testing the wordpress side

--- compo/plugins/ldjam/validate-user.php
+++ compo/plugins/ldjam/validate-user.php
@@ -1,13 +1,17 @@
 <?php
 // Check wordpress credentials //
+require_once __DIR__."/../../../wp-load.php";
+
+echo wp_get_current_user();
 
 // Set Cookie //
-setcookie( "lusha", "1234", time()+2*24*60*60, "/", ".ludumdare.com" );
+//setcookie( "lusha", "1234", time()+2*24*60*60, "/", ".ludumdare.com" );
 
 // Redirect //
-header("Location: http://theme.ludumdare.com");
+//header("Location: http://theme.ludumdare.com");
 
 //echo '<!doctype html>';
 //echo '<html><head><meta http-equiv="Location" content="http://example.com/"></head>';
 //echo '<body><a href="http://theme.ludumdare.com">http://theme.ludumdare.com</a></body></html>';
+
 die();
