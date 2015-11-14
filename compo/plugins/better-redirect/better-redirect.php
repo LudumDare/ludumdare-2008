f339c862a1afe06fdeced9094ed9cda8e4d0fa18
Mike Kasprzak <mike@sykhronics.com>
2015-11-14 11:18:46 -0500

    Added my BETTER redirect plugin... I wonder if it'll work :D

--- compo/plugins/better-redirect/better-redirect.php
+++ compo/plugins/better-redirect/better-redirect.php
@@ -1,8 +1,14 @@
-/**
- * Redirect non-admins to the homepage after logging into the site.
- *
- * @since 	1.0
- */
+<?php
+/*
+Plugin Name: better-redirect
+Plugin URI: http://ludumdare.com/
+Description: A better redirect plugin. No stupid stuff. Actually aware of WP. 
+Version: 1.0
+Author: Mike Kasprzak
+Author URI: http://www.sykhronics.com
+License: BSD
+*/
+// - ----------------------------------------------------------------------------------------- - //
 function acme_login_redirect( $redirect_to, $request, $user  ) {
 	if ( $request )
 		return $request;
