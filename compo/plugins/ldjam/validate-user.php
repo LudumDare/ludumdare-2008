53987261acbaa5d03681c94ca1d883b6c5737037
Mike Kasprzak <mike@sykhronics.com>
2015-11-13 19:29:09 -0500

    Ha, I mixed up PHP and Javascript syntax

--- compo/plugins/ldjam/validate-user.php
+++ compo/plugins/ldjam/validate-user.php
@@ -68,7 +68,7 @@ if ( isset($user->ID) && ($user->ID > 0) ) {
 else {
 	// TODO: Include data for WP-login redirect to send it back here after logging in //
 	header("Location: http://ludumdare.com/compo/wp-login.php?".
-		http_build_query({'redirect_to' => "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}"})
+		http_build_query(['redirect_to' => "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}"])
 	);
 	die();	
 }
